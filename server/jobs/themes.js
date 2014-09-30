a5f4c0b6e243fe8143b740cfc8a770827ed659f7
laurent-muchacho <laurent.muchacho@ft.com>
2014-09-30 16:45:39 +0100

    removed inline function from inside the loop

--- server/jobs/themes.js
+++ server/jobs/themes.js
@@ -8,34 +8,14 @@ var ft = require('ft-api-client')(process.env.apikey),
 
     var themes_article = [];
 
-
-
-        // ft
-        // .search(decodeURI(req.query.q), count)
-        // .then(function (articles) {
-
-        //     var ids = articles.map(function (article) {
-        //         return article.id;
-        //     });
-
-        //     ft
-        //         .get(ids)
-        //         .then( function (articles) {
-        //             res.render('layout/base', {
-        //                 mode: 'compact',
-        //                 stream: articles,
-        //                 context: formatSection(req.query.q)
-        //             });
-        //         }, function(err) {
-        //             console.log(err);
-        //             res.send(404);
-        //         });
-
-        // }, function (err) {
-        //     console.log(err);
-        //     res.send(404);
-        // });
-
+function processArticle(theme) {
+  return function (articles) {
+    theme.articles = articles;
+  };
+}
+function processError(err) {
+  console.log(err);
+}
 
 module.exports = {
 
@@ -44,24 +24,11 @@ module.exports = {
     },
 
     init: function() {
-      // console.log('themes init: ');
-        //       ft
-        // .search('SCOTTISH INDEPENDENCE', 10)
-        // .then(function (articles) {
-        //   console.log('article: ', articles);
-        //   // themes_article.push({
-        //   //   name: theme,
-        //   //   articles: articles
-        //   // });
 
-        // }, function (err) {
-        //   console.log(err);
-        // });
 
       for (var i = 0, l = themes.length; i < l; i += 1) {
         var theme = themes[i];
         // console.log('theme: ', theme);
-
 
         var index = themes_article.push({
           name: theme
@@ -69,15 +36,7 @@ module.exports = {
 
         ft
         .search(decodeURI(theme), 3)
-        .then((function(theme){
-          return function (articles) {
-          // console.log('article: ', articles);
-          theme.articles = articles;
-
-        }
-        })(themes_article[index-1]), function (err) {
-          console.log(err);
-        });
+        .then(processArticle(themes_article[index-1]), processError);
       }
     }
 };
