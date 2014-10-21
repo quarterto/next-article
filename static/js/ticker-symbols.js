ece88c0c768258c2083393d6c1ca8f6909df58fa
Matt Chadburn <matt.chadburn@ft.com>
2014-10-21 11:59:30 +0100

    Render market data in-stream

---
+++ static/js/ticker-symbols.js
@@ -0,0 +1,24 @@
+
+// Enhances any ticker symbol found on the page with live pricing data
+function init () {
+
+
+    console.log('prog enhance ticker');
+
+    var host = 'http://next-companies-et-al.herokuapp.com/v1/mouseover/company/';
+    var symbols = document.querySelectorAll('.article-card__ticker-symbols__item');
+
+    [].slice.call(symbols).forEach(function (symbol) {
+        var code = symbol.getAttribute('data-ticker-symbols');
+        reqwest(host + code, function (resp) {
+            // did the market price go up?
+            var isUp = (parseFloat(resp.data.latest_change.percentage) >= 0) ? 'up' : 'down'; 
+            var price = resp.data.latest_price.amount;
+            var change = resp.data.latest_change.percentage;
+            symbol.innerHTML = '<span class="article-card__ticker-symbols__item-' + isUp + '">' + resp.data.returned_symbol + ' ' + price + ' (' + change  + ')</span>';
+        })
+    })
+
+}
+
+init();
