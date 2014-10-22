665e8f4c82c70d49332395ce2527a1829cd70a09
Matt Chadburn <matt.chadburn@ft.com>
2014-10-22 11:05:25 +0100

    Streams
    Dobi now has a concept of a Stream, which essentially is a list of _types of
    content._
    
    This allows us to build streams with various types of content - video,
    articles, fastFT, blogs, external content - and give
    visual/functionality treatment to each one.

---
+++ models/stream.js
@@ -0,0 +1,25 @@
+
+
+/*
+
+    A stream is a list of content of various types,
+
+    [
+      { type: 'methode,   item: { uuid: ... } },
+      { type: 'fastft',   item: { uuid: ... } },
+      { type: 'external', item: { uuid: ... } }
+    ]
+
+*/
+
+var Stream = function () {
+    this.items = [];
+}
+
+Stream.prototype.push = function (type, item) {
+    this.items.push({ type: type, item: item });
+}
+
+Stream.prototype.sortByDate = function () { }
+
+module.exports = Stream;
