e0ec8a8a2ce30c340d3e21abfed98097120d07fd
Arjun Gadhia <arjungadhia@gmail.com>
2014-10-29 11:21:11 +0000

    Remove sort by date since its not being used

--- models/stream.js
+++ models/stream.js
@@ -24,18 +24,6 @@ Stream.prototype.push = function (type, item) {
     this.items.push({ type: type, item: item });
 }
 
-Stream.prototype.sortByLastPublished = function () { 
-	this.items.sort(function(a, b) {
-		if(a.item.lastUpdated.getTime() > b.item.lastUpdated.getTime()) {
-			return -1
-		} else if (a.item.lastUpdated.getTime() === b.item.lastUpdated.getTime()) {
-			return 0;
-		} else {
-			return 1;
-		}
-	});
-}
-
 
 //Puts greater emphasis on comment and analysis
 // Latest news article first, followed by all comment and analysis, followed by rest of news
