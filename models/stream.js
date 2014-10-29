497f959743135d8a4ab1677f9fb8b25138f3121d
Arjun Gadhia <arjungadhia@gmail.com>
2014-10-29 11:01:27 +0000

    Sort by visual tone and date

--- models/stream.js
+++ models/stream.js
@@ -12,6 +12,16 @@
 
 */
 
+function isPromoted(item) {
+	var promote = ['comment', 'analysis'];
+
+	if(promote.indexOf(item) >= 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 var Stream = function () {
     this.items = [];
 }
@@ -20,6 +30,42 @@ Stream.prototype.push = function (type, item) {
     this.items.push({ type: type, item: item });
 }
 
-Stream.prototype.sortByDate = function () { }
+Stream.prototype.sortByLastPublished = function () { 
+	this.items.sort(function(a, b) {
+		if(a.item.lastUpdated.getTime() > b.item.lastUpdated.getTime()) {
+			return -1
+		} else if (a.item.lastUpdated.getTime() === b.item.lastUpdated.getTime()) {
+			return 0;
+		} else {
+			return 1;
+		}
+	});
+}
 
+
+//Puts greater emphasis on comment and analysis
+// Latest news article first, followed by all comment and analysis, followed by rest of news
+Stream.prototype.sortByToneAndLastPublished = function() {
+	var latest = this.items.shift();
+
+	this.items.sort(function(a, b) {
+
+		if(isPromoted(a.item.visualTone) && !isPromoted(b.item.visualTone)) {
+			return -1;
+		} else if (isPromoted(a.item.visualTone) === isPromoted(b.item.visualTone)) {
+			if(a.item.lastUpdated.getTime() > b.item.lastUpdated.getTime()) {
+				return -1
+			} else if (a.item.lastUpdated.getTime() === b.item.lastUpdated.getTime()) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} else {
+			return 1;
+		}
+	});
+
+	this.items.unshift(latest);
+
+}
 module.exports = Stream;
