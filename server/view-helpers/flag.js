ed8260ea58dfb4dcc849fc63a9488b0a6ef7ff8c
Rhys Evans <rhys.evans@ft.com>
2014-10-31 17:16:45 +0000

    Now actually works for negated flags

--- server/view-helpers/flag.js
+++ server/view-helpers/flag.js
@@ -1,28 +1,34 @@
 'use strict';
 
-var Flags = require('./utils/flags');
+var Flags = require('../utils/flags');
 
 require('swig').setTag('flag', function (str, line, parser, types, options, swig) {
     var flags;
     parser.on('start', function () {
         flags = Flags.get();
     });
+    parser.on('*', function (token) {
+        token.match === '!' && this.out.push(token.match);
+    });
     parser.on(types.VAR, function (token) {
-        var flag = token.match;
-        var negate = false;
-        
-        if (flag.charAt(0) == '!') {
-            negate = true;
-            flag = flag.substr(1);
-        }
-        flag = flags[flag];
-
-        var isOn = flag && flag.isSwitchedOn();
-        this.out.push(negate ? !isOn : isOn);
+        this.out.push(token.match);
+        this.out.push(flags);
     });
     return true;
 }, function (compiler, args, content, parents, options, blockName) {
-    if (args[0]) {
+    
+    var negate = args[0] === '!';
+    
+    if (negate) { 
+        args = args.slice(1);
+    }
+    
+    var flag = args[0];
+    var flags = args[1]
+    var isOn = flags[flag] && flags[flag].isSwitchedOn();
+    var willRender = negate ? !isOn : isOn;
+
+    if (willRender) {
         return compiler(content, parents, options, blockName);
     } else {
         return ';';
