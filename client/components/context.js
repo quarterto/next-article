476494f684a87b758b823f2734f3c23c7adbc766
Arjun Gadhia <arjungadhia@gmail.com>
2014-11-07 12:02:15 +0000

    Delete unused client side code

--- client/components/context.js
+++ client/components/context.js
@@ -7,10 +7,6 @@ function setContext(path, display) {
     localStorage.setItem(contextTitleKey, display);
 }
 
-var onArticle = function (path) {
-    return /^\/[a-f0-9]+-(.*)/.test(path); // '27a5e286-4314-11e4-8a43-00144feabdc0'; 
-};
-
 var $ = function (selector) {
     return [].slice.call(document.querySelectorAll(selector));
 };
@@ -33,27 +29,19 @@ var contextKey = 'ft.stream.context.url';
 var contextTitleKey = 'ft.stream.context.display';
 var display, context;
 
-/* 1. in stream mode store the context URL and content display name */
-if (!onArticle(location.pathname)) {
-    // Every time you hit a new stream, you enter a new context
-    context = extractSearchTerm(location.search);
-    display = document.getElementsByClassName('js-context')[0].textContent.trim();
+/* 1. in article mode store a default context if it doesnt exist */
+
+context = localStorage.getItem(contextKey);
+display = localStorage.getItem(contextTitleKey);
+if(!context) { 
+    //If they come directly to an article with no history, use the first theme for this article
+    context = extractSearchTerm(document.querySelector('.article-card__themes a').getAttribute('href'));
+    display = document.querySelector('.article-card__themes a').textContent.trim();
     setContext(context, display);
-    localStorage.setItem(contextTitleKey, display);
-    emit('stream:open', {id: context});
-} else {
-    context = localStorage.getItem(contextKey);
-    display = localStorage.getItem(contextTitleKey);
-    if(!context) { 
-        //If they come directly to an article with no history, use the first theme for this article
-        context = extractSearchTerm(document.querySelector('.article-card__themes a').getAttribute('href'));
-        display = document.querySelector('.article-card__themes a').textContent.trim();
-        setContext(context, display);
-    }
 }
 
 /* 2. in article view render the context menu full mode */    
-if (onArticle(location.pathname) && context) {
+if (context) {
     $('.js-context').map(function (el) {
         el.innerHTML = localStorage.getItem(contextTitleKey);
     });
--- client/components/uber-nav/main.js
+++
@@ -1,5 +0,0 @@
-var reqwest = require('../../vendor/reqwest.min');
-reqwest('/uber-nav', function(resp) {
-    document.querySelector('.js-splash').innerHTML = resp;
-});
-
