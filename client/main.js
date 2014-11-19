4e8ca92361de19ce6492354d3ccd4b1081587b9d
Rhys Evans <rhys.evans@ft.com>
2014-11-19 11:51:49 +0000

    feature flagging

--- client/main.js
+++ client/main.js
@@ -1,9 +1,19 @@
+var flags = window.FT.flags;
+
 require('next-wrapper')();
 require('next-actions-component');
 require('next-article-card-component');
 require('./components/context');
-require('./components/more-on/main');
-require('./components/on-this-topic/main');
-require('./components/live-prices/main');
-require('./components/video');
+if (flags.contentApiCalls.isSwitchedOn) {
+    require('./components/more-on/main');
+    require('./components/on-this-topic/main');
+}
+
+if (flags.marketData.isSwitchedOn) {
+    require('./components/live-prices/main');
+}
+
+if (flags.video.isSwitchedOn) {
+    require('./components/video');
+}
 
