2dd330045097585a3ac3679b681306b0a4870878
Arjun Gadhia <arjungadhia@gmail.com>
2014-11-20 12:00:17 +0000

    Update flag stuff and remove next-actions-component

--- client/main.js
+++ client/main.js
@@ -1,19 +1,10 @@
 var flags = window.FT.flags;
 
 require('next-wrapper')();
-require('next-actions-component');
+require('next-user-preferences');
 require('next-article-card-component');
 require('./components/context');
 if (flags.contentApiCalls.isSwitchedOn) {
     require('./components/more-on/main');
     require('./components/on-this-topic/main');
 }
-
-if (flags.marketData.isSwitchedOn) {
-    require('./components/live-prices/main');
-}
-
-if (flags.video.isSwitchedOn) {
-    require('./components/video');
-}
-
