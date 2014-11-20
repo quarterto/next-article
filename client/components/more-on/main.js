aad5d07fe022c47e545ea8d490c494091a413d18
Arjun Gadhia <arjungadhia@gmail.com>
2014-11-20 12:08:15 +0000

    Update gulpfile (and sneak in more-on css fix)

--- client/components/more-on/main.js
+++ client/components/more-on/main.js
@@ -7,6 +7,7 @@ var $ = function (selector) {
 $('.js-more-on').forEach(function (el) {
     reqwest('/more-on/' + el.getAttribute('data-article-id'))
         .then(function (resp) {
+            el.classList.add('more-on--loaded');
             el.innerHTML = resp;
             if (window.Origami) {
                 window.Origami['o-date'].init(el);
