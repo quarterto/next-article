1b9d0b9f585ca21a9676258c512f002dc3721b72
Matthew Andrews <matt@mattandre.ws>
2014-11-21 11:35:38 +0000

    Better error handling; ditch Promise.prototype.end

--- server/controllers/more-on-topic.js
+++ server/controllers/more-on-topic.js
@@ -3,65 +3,60 @@
 var ft = require('../utils/api').ft;
 
 var titleMapping = {
-    'primarySection': 'section',
-    'primaryTheme': 'theme'
+	'primarySection': 'section',
+	'primaryTheme': 'theme'
 };
 
 module.exports = function(req, res, next) {
-    ft.get([req.params.id])
-        .then(function(thisArticle) {
-            var topic, query, topicTitle = titleMapping[req.params.metadata];
-            if(thisArticle && thisArticle.length) {
-                thisArticle = thisArticle[0];
-            } else {
-                res.status(404).send();
-                return;
-            }
-            topic = thisArticle[req.params.metadata];
-            if(!topic) {
-                res.status(404).send();
-                return;
-            }
+	ft.get([req.params.id])
+		.then(function(thisArticle) {
+			var topic, query, topicTitle = titleMapping[req.params.metadata];
+			if (thisArticle && thisArticle.length) {
+				thisArticle = thisArticle[0];
+			} else {
+				res.status(404).send();
+				return;
+			}
+			topic = thisArticle[req.params.metadata];
+			if (!topic) {
+				res.status(404).send();
+				return;
+			}
 
-            query = topic.taxonomy + ':' + topic.name;
+			query = topic.taxonomy + ':' + topic.name;
 
-            ft.search(query, 4)
-            .then(function (results) {
-                var ids;
-                var articles = results ? results.articles : [];
-                if (articles[0] instanceof Object) {
-                    ids = articles.map(function (article) {
-                        return article.id;
-                    });
-                } else {
-                    ids = articles; // FIXME when is this ever not a Object?
-                }
-                ft.get(ids)
-                    .then( function (articles) {
-                        articles = articles.filter(function(article) {
-                            return article.id !== thisArticle.id;
-                        });
+			return ft.search(query, 4)
+				.then(function (results) {
+					var ids;
+					var articles = results ? results.articles : [];
+					if (articles[0] instanceof Object) {
+						return articles.map(function (article) {
+							return article.id;
+						});
+					}
+					return [];
+				})
+				.then(function(ids) {
+					return ft.get(ids)
+				})
+				.then(function (articles) {
+					articles = articles.filter(function(article) {
+						return article.id !== thisArticle.id;
+					});
 
-                    if (articles.length > 0) {
-                        require('../utils/cache-control')(res);
-                        res.render('components/more-on', {
-                            mode: 'expand',
-                            stream: articles,
-                            query: query,
-                            title: 'More from this ' + topicTitle + ' - ' + topic.name
-                        });
-                    } else {
-                        res.status(404).send();
-                        return;
-                    }
-                }, function (err) {
-                    console.error(err);
-                });
-
-            }, function (err) {
-                console.error(err);
-            });
-    }, function (err) {
-        console.error(err);
-    }).end();
+					if (articles.length > 0) {
+						require('../utils/cache-control')(res);
+						res.render('components/more-on', {
+							mode: 'expand',
+							stream: articles,
+							query: query,
+							title: 'More from this ' + topicTitle + ' - ' + topic.name
+						});
+					} else {
+						res.status(404).send();
+						return;
+					}
+				})
+		})
+		.catch(next);
 };
