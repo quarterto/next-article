03588c4189231916b8bc81390eedeb401de3b3eb
Arjun Gadhia <arjungadhia@gmail.com>
2014-11-21 16:38:56 +0000

    Remove context stuff and use section as title

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ var flags = window.FT.flags;
 require('next-wrapper')();
 require('next-user-preferences');
 require('next-article-card-component');
-require('./components/context');
 if (flags.contentApiCalls.isSwitchedOn) {
     require('./components/more-on/main');
 }
