aed96df9e306b86b4d533201622c4f32566b04c9
Matt Chadburn <matt.chadburn@ft.com>
2014-12-10 22:48:11 +0000

    Include a sample beacon call when a page loads

--- client/main.js
+++ client/main.js
@@ -3,6 +3,21 @@ var flags = window.FT.flags;
 require('next-wrapper')();
 require('next-user-preferences');
 require('next-article-card-component');
+
 if (flags.contentApiCalls.isSwitchedOn) {
     require('./components/more-on/main');
+}
+
+
+if (flags.beacon.isSwitchedOn) {
+
+    var Beacon = require('next-beacon-component');
+
+    // fire a beacon after 5s of being on the page
+    setTimeout(function () {
+        Beacon.fire('click', {
+            uuid: location.pathname.slice(1)
+        });
+    }, 5000);
+
 }
