a0fce75cd1ad2717ca37f4a5ca5e76fb940ab506
Matthew Andrews <matt@mattandre.ws>
2014-12-17 17:32:08 +0000

    More updates to make moreons work a bit better

--- server/controllers/more-on.js
+++ server/controllers/more-on.js
@@ -6,14 +6,12 @@ var Metrics = require('next-metrics');
 var Stream = require('../models/stream');
 var cacheControl = require('../utils/cache-control');
 
-
 module.exports = function(req, res, next) {
-
 	Metrics.instrument(res, { as: 'express.http.res' });
 
 	ft
 		.get([req.params.id])
-		.then(function (article) {
+		.then(function(article) {
 			if (article[0] && article[0].packages && article[0].packages.length > 0) {
 				article = article[0];
 			} else {
@@ -23,11 +21,9 @@ module.exports = function(req, res, next) {
 
 			return ft
 				.get(article.packages)
-				.then(function (articles) {
+				.then(function(articles) {
 					if (articles.length > 0) {
-
 						var stream = new Stream();
-
 						articles.forEach(function(item) {
 							stream.push('methode', item);
 						});
@@ -40,9 +36,6 @@ module.exports = function(req, res, next) {
 					} else {
 						res.status(404).send();
 					}
-				}, function (err) {
-					console.log(err);
-					res.send(404);
 				});
 		})
 		.catch(next);
