6e97001ec5539552c1b5e81540f36bad51a8c89d
Matthew Andrews <matt@mattandre.ws>
2015-01-13 17:07:39 +0000

    Simplify by returning a string

--- server/transforms/pull-quotes.js
+++ server/transforms/pull-quotes.js
@@ -2,9 +2,9 @@ var $ = require('cheerio');
 module.exports = function(index, el) {
 	var text = $(el).find('pull-quote-text').text();
 	var cite = $(el).find('pull-quote-source').text();
-	return $('<blockquote class="o-quote o-quote--standard"><p>'
+	return '<blockquote class="o-quote o-quote--standard"><p>'
 		+ text
 		+ '</p><cite class="o-quote__cite">'
 		+ cite
-		+ '</cite></blockquote>');
+		+ '</cite></blockquote>';
 };
