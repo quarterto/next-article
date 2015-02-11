026aeb8bda4a2e04163119c394c6e26b966dbdfb
Matthew Andrews <matt@mattandre.ws>
2015-02-11 13:57:24 +0000

    Restore “See alsos”

--- client/components/more-on/main.js
+++ client/components/more-on/main.js
@@ -2,11 +2,10 @@
 'use strict';
 
 var articleCard = require('next-article-card-component');
-
-
+var fetchres = require('fetchres');
 
 // Sort of like Promise.all but will be called whether they fail or succeed
-Promise.allSettled = function(promises){
+function allSettled(promises) {
 	var resolveWhenSettled = function(promise) {
 		return new Promise(function(res) {
 			promise.then(res, function() {
@@ -17,10 +16,10 @@ Promise.allSettled = function(promises){
 	return Promise.all(promises.map(resolveWhenSettled));
 };
 
-var initAds = function(flags){
+var initAds = function(flags) {
 	var called = false;
-	return function(){
-		if(!called && flags.ads.isSwitchedOn){
+	return function() {
+		if (!called && flags.ads.isSwitchedOn){
 			var ads = require('next-ads-component');
 			ads.init();
 			called = true;
@@ -28,43 +27,35 @@ var initAds = function(flags){
 	};
 };
 
-var fetchText = function(url) {
-	return fetch(url)
-		.then(function(response) {
-			if (response.status >= 400 && response.status < 600) {
-				throw new Error("Bad response from server for " + url);
-			}
-			return response.text();
-		});
-};
-
-var $ = function (selector) {
+var $ = function(selector) {
 	return [].slice.call(document.querySelectorAll(selector));
 };
 
-var fetchPromises = [];
-
-$('.js-more-on').forEach(function (el) {
-	fetchPromises.push(fetchText('/more-on/' + el.getAttribute('data-article-id'))
-		.then(function (resp) {
-			el.innerHTML = resp;
-			articleCard.init(el);
-		}, function() {
-			el.parentNode.removeChild(el);
-		}));
-});
-
-$('.js-on-this-topic').forEach(function (el) {
-	fetchPromises.push(fetchText('/more-on/' + el.getAttribute('data-metadata-field') + '/' + el.getAttribute('data-article-id'))
-		.then(function (resp) {
-			el.innerHTML = resp;
-			articleCard.init(el);
-		}, function() {
-			el.parentNode.removeChild(el);
-		}));
-});
 
 module.exports.init = function(flags){
-	Promise.allSettled(fetchPromises).then(initAds(flags));
-};
+	var fetchPromises = [];
 
+	$('.js-more-on').forEach(function(el) {
+		fetchPromises.push(fetch('/more-on/' + el.getAttribute('data-article-id'))
+			.then(fetchres.text)
+			.then(function(resp) {
+				el.innerHTML = resp;
+				articleCard.init(el);
+			}, function() {
+				el.parentNode.removeChild(el);
+			}));
+	});
+
+	$('.js-on-this-topic').forEach(function(el) {
+		fetchPromises.push(fetchText('/more-on/' + el.getAttribute('data-metadata-field') + '/' + el.getAttribute('data-article-id'))
+			.then(function(resp) {
+				el.innerHTML = resp;
+				articleCard.init(el);
+			}, function() {
+				el.parentNode.removeChild(el);
+			}));
+	});
+
+	return allSettled(fetchPromises)
+		.then(initAds(flags));
+};
