484ad3fb1ab3fabf55a0554a27308fa5bfb95db7
Matthew Andrews <matt@mattandre.ws>
2015-02-11 21:13:45 +0000

    places -> regions

--- server/transforms/relative-links.js
+++ server/transforms/relative-links.js
@@ -4,6 +4,7 @@
 var $ = require('cheerio');
 module.exports = function(index, el) {
 	el = $(el);
+	el.attr('href', el.attr('href').replace(/https?:\/\/(?:www\.)?ft\.com\/topics\/places/, '/stream/regions'));
 	el.attr('href', el.attr('href').replace(/https?:\/\/(?:www\.)?ft\.com\/topics/, '/stream'));
 	el.attr('href', el.attr('href').replace(/https?:\/\/(?:www\.)?ft\.com\/cms(?:\/s\/[0-9])?\/([a-zA-Z0-9-]+)\.html/, '/$1'));
 	if (/^\/stream\//.test(el.attr('href'))) {
