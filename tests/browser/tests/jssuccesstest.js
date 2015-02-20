6bfa4919e9a536405ea5aa06908a639d5f6a4513
Harry Spriggs <harry.spriggs@ft.com>
2015-02-20 11:30:17 +0000

    another url fix

--- tests/browser/tests/jssuccesstest.js
+++ tests/browser/tests/jssuccesstest.js
@@ -9,7 +9,7 @@ module.exports = {
 	"Js-success Test" : function (browser) {
 		console.log('Testing host', browser.launch_url+"b70cc2d6-b809-11e4-b6a5-00144feab7de");
 		browser
-			.url(browser.launch_url+"/b70cc2d6-b809-11e4-b6a5-00144feab7de")
+			.url(browser.launch_url+"b70cc2d6-b809-11e4-b6a5-00144feab7de")
 			.waitForElementVisible('body', 5000)
 			.assert.attributeEquals(".js", "class", " js")
 			.end();
