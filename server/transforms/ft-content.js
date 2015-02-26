685d8ead06802043a2bc552bb6e6806d93866717
Matthew Andrews <matt@mattandre.ws>
2015-02-26 13:52:05 +0000

    Deal with unrecognised ft-content

--- server/transforms/ft-content.js
+++ server/transforms/ft-content.js
@@ -14,5 +14,7 @@ module.exports = function(index, el) {
 			return '<img src="/embedded-components/image' + url + '"/ >';
 		case 'http://www.ft.com/ontology/content/Article':
 			return '<a href="' + url + '">' + text + '</a>';
+		default:
+			return '';
 	}
 };
