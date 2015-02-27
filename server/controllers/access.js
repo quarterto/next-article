0532fde0c5ea6c19fb99dc92852afeb592a51d48
Paul I Wilson <paul.i.wilson@ft.com>
2015-02-27 10:50:10 +0000

    caching headers

--- server/controllers/access.js
+++ server/controllers/access.js
@@ -10,6 +10,8 @@ module.exports = function(req, res, next){
 	var api = require('ft-api-client')(apiKey);
 	if(req.get('X-FT-Access-Metadata') === 'remote_headers'){
 		api.get(req.params[0]).then(function(article){
+			res.set('Cache-Control', 'public, max-age=3600, s-maxage=3600');
+			res.vary('X-FT-UID');
 			res.set('X-FT-UID', article.id);
 			res.set('X-FT-Content-Classification', article.contentClassification);
 			res.status(200).end();
