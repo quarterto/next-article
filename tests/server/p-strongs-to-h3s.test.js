7def53038cee9b410f4257b621303fce6e8702ae
Matthew Andrews <matt@mattandre.ws>
2015-03-04 11:49:35 +0000

    Unexpected spaces

--- tests/server/p-strongs-to-h3s.test.js
+++ tests/server/p-strongs-to-h3s.test.js
@@ -5,6 +5,6 @@ var expect = require('chai').expect;
 var pStrongsToH3s = require('../../server/transforms/p-strongs-to-h3s');
 
 it('should convert `<p><strong>...</p></strong>` to `<h3>...</h3>`', function() {
-    var transformed = pStrongsToH3s('<p><strong>London in a world of its own</strong></p>');
-    expect(transformed).to.equal('<h3 class="article__subheader">London in a world of its own</h3>');
+	var transformed = pStrongsToH3s('<p><strong>London in a world of its own</strong></p>');
+	expect(transformed).to.equal('<h3 class="article__subheader">London in a world of its own</h3>');
 });
