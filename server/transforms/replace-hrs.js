80c69651288b10d2f5a2de73db47ed9d9062ad7e
Matthew Andrews <matt@mattandre.ws>
2015-03-05 16:52:46 +0000

    Cope with editorial bolding the *'s

--- server/transforms/replace-hrs.js
+++ server/transforms/replace-hrs.js
@@ -1,4 +1,4 @@
 'use strict';
 module.exports = function(body) {
-	return body.replace(/<p>[-*]{3,}<\/p>/, '<hr />');
+	return body.replace(/<p>\s*(?:<strong>)?[-*]{3,}(?:<\/strong>)?\s*<\/p>/, '<hr />');
 };
