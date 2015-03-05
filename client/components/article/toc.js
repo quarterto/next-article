d5f7de2cdb4494dbca0fa7e409e81748100cc819
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-05 15:05:29 +0000

    position of toc; easing

--- client/components/article/toc.js
+++ client/components/article/toc.js
@@ -5,10 +5,24 @@ module.exports.init = function (flags) {
 		return false;
 	}
 
-	var toc = document.querySelector('.article__toc__items');
+	var duration = 1000,
+		// https://gist.github.com/gre/1650294
+		easeFunction = function (t) { return 1-(--t)*t*t*t };
 
-	toc.addEventListener('change', function (event) {
-		// get the section and update #
-		window.location.hash = event.target.value;
+	document.querySelector('.article__toc__items').addEventListener('change', function (event) {
+
+		var scrollFrom = document.body.scrollTop,
+			scrollDist = document.querySelector('#' + event.target.value).getBoundingClientRect().top,
+			startTime  = new Date(),
+			interval   = window.setInterval(function () {
+				var elapsed = new Date() - startTime;
+
+				document.body.scrollTop = scrollFrom + (easeFunction(elapsed / duration) * scrollDist);
+			}, 15);
+
+		window.setTimeout(function () {
+			window.clearInterval(interval);
+			document.body.scrollTop = scrollTo;
+		}, duration);
 	});
 };
