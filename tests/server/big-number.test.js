94a358492716f989a8116d4c49e520d946614877
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-11 15:59:43 +0000

    use latest o-big-colors

--- tests/server/big-number.test.js
+++ tests/server/big-number.test.js
@@ -7,5 +7,5 @@ var bigNumberTransform = require('../../server/transforms/big-number');
 it('should understand that topic pages are stream pages', function() {
 	var $ = cheerio.load('<big-number><big-number-headline>33m</big-number-headline><big-number-intro>These are powerful but fragile emissaries of a culture that not even their descendants remember</big-number-intro></big-number>');
 	$('big-number').replaceWith(bigNumberTransform);
-	expect($.html()).to.equal('<span class="article__big-number ng-pull-out o-big-number o-big-number--standard"><span class="o-big-number__title">33m</span><span class="o-big-number__content">These are powerful but fragile emissaries of a culture that not even their descendants remember</span></span>');
+	expect($.html()).to.equal('<span class="article__big-number ng-pull-out ng-inline-element o-big-number o-big-number--standard"><span class="o-big-number__title">33m</span><span class="o-big-number__content">These are powerful but fragile emissaries of a culture that not even their descendants remember</span></span>');
 });
