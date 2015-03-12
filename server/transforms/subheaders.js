b870bb8f74afa7e26e977022697d71921b5dc26a
Matthew Andrews <matt@mattandre.ws>
2015-03-12 16:01:59 +0000

    Add tracking to the ‘back to top’ icon

--- server/transforms/subheaders.js
+++ server/transforms/subheaders.js
@@ -7,7 +7,7 @@ module.exports = function(index, el) {
 
 	$el.addClass('article__subhead ng-pull-out')
 		.html('<span class="ft-subhead__title">' + $el.text() + '</span>')
-		.append('<a class="back-top-top" href="#top"><span class="back-top-top__text">Back to top</span><span class="back-top-top__icon" /></a>');
+		.append('<a class="back-top-top" href="#top" data-trackable="back-to-top"><span class="back-top-top__text">Back to top</span><span class="back-top-top__icon" /></a>');
 
 	return $el.clone();
 };
