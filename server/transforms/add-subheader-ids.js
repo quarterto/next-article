8d590aee158377fd66221919c53c0d728ef45f48
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-16 15:41:27 +0000

    don't try and convert text into id

--- server/transforms/add-subheader-ids.js
+++ server/transforms/add-subheader-ids.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var $ = require('cheerio');
-
-module.exports = function(index, oldId) {
-	return $(this).text().trim().toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]/g, '');
+module.exports = function(index) {
+	return 'subhead-' + (index + 1);
 };
