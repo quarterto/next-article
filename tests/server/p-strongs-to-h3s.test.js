bec4ffeaaf4b0a96962ca148ed248957beb8082d
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-16 14:29:40 +0000

    convert all occurrences

--- tests/server/p-strongs-to-h3s.test.js
+++ tests/server/p-strongs-to-h3s.test.js
@@ -8,3 +8,8 @@ it('should convert `<p><strong>...</p></strong>` to `<h3>...</h3>`', function() 
 	var transformed = pStrongsToH3s('<p><strong>London in a world of its own</strong></p>');
 	expect(transformed).to.equal('<h3 class="ft-subhead">London in a world of its own</h3>');
 });
+
+it('should convert multiple occurrences', function() {
+	var transformed = pStrongsToH3s('<p><strong>London in a world of its own</strong></p><p><strong>Paying a heavy price</strong></p>');
+	expect(transformed).to.equal('<h3 class="ft-subhead">London in a world of its own</h3><h3 class="ft-subhead">Paying a heavy price</h3>');
+});
