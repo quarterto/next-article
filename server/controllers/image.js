f06bf518be794725e7df54dbcde4411727646ad8
Matthew Andrews <matt@mattandre.ws>
2015-03-16 10:25:13 +0000

    fix small image regression

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -4,15 +4,10 @@ var fetchres = require('fetchres');
 var fetchCapiV2 = require('../utils/fetch-capi-v2');
 
 module.exports = function(req, res, next) {
-	fetchCapiV2(req.params.id)
+	fetchCapiV2({ uuid: req.params.id })
 		.then(fetchres.json)
 		.then(function(image) {
-			return fetch(image.members[0].id.replace('http://api.ft.com/content/', ''), {
-					timeout: 3000,
-					headers: {
-						'X-Api-Key': process.env.api2key
-					}
-				})
+			return fetchCapiV2({ uuid: image.members[0].id.replace('http://api.ft.com/content/', '') })
 				.then(fetchres.json);
 		})
 		.then(function(image) {
