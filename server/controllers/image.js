72ca807d0419d1a714209eddad7ec70268bbca79
Matthew Andrews <matt@mattandre.ws>
2015-03-16 09:48:41 +0000

    Centralise CAPI v2 util

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,33 +1,29 @@
 'use strict';
 
 var fetchres = require('fetchres');
+var fetchCapiV2 = require('../utils/fetch-capi-v2');
 
 module.exports = function(req, res, next) {
-	fetch('http://api.ft.com/content/' + req.params.id, {
-		timeout: 3000,
-		headers: {
-			'X-Api-Key': process.env.api2key
-		}
-	})
-	.then(fetchres.json)
-	.then(function(image) {
-		return fetch(image.members[0].id, {
-				timeout: 3000,
-				headers: {
-					'X-Api-Key': process.env.api2key
-				}
-			})
-			.then(fetchres.json);
-	})
-	.then(function(image) {
-		res.redirect(image.binaryUrl);
-	})
-	.catch(function(err) {
-		if (err instanceof fetchres.BadServerResponseError) {
-			res.status(404).end();
-		} else {
-			throw err;
-		}
-	})
-	.catch(next);
+	fetchCapiV2(req.params.id)
+		.then(fetchres.json)
+		.then(function(image) {
+			return fetch(image.members[0].id.replace('http://api.ft.com/content/', ''), {
+					timeout: 3000,
+					headers: {
+						'X-Api-Key': process.env.api2key
+					}
+				})
+				.then(fetchres.json);
+		})
+		.then(function(image) {
+			res.redirect(image.binaryUrl);
+		})
+		.catch(function(err) {
+			if (err instanceof fetchres.BadServerResponseError) {
+				res.status(404).end();
+			} else {
+				throw err;
+			}
+		})
+		.catch(next);
 };
---
+++ server/utils/fetch-capi-v2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(opts) {
+	var uuid = opts.uuid;
+	var url = 'http://api.ft.com/content/' + uuid + '?sjl=WITH_RICH_CONTENT';
+
+	return fetch(url, {
+			timeout: 3000,
+			headers: {
+				'X-Api-Key': process.env.apikey
+			}
+		});
+};
