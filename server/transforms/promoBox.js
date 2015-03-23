6d14cce09e49e70378672d230ee4a1636d3b6113
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-23 12:47:52 +0000

    transform promobxes

---
+++ server/transforms/promoBox.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var $ = require('cheerio');
+
+module.exports = function(index, el) {
+	var $el = $(el);
+	var $promoBox = $('<div></div')
+		.addClass('article__promobox ng-pull-out ng-inline-element');
+	var title = $('<h3></h3>')
+		.addClass('article__promobox__title')
+		.text($el.find('promo-title').text());
+	var image = $el.find('promo-image')
+		.html();
+	var content = $el.find('promo-intro').html();
+
+	return $promoBox
+		.append(title)
+		.append(image)
+		.append(content);
+};
