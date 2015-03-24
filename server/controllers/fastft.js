7915c1659e779b27ffdda032d4ecc0144b0eb8cf
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-24 15:46:20 +0000

    tidying fastft tests

--- server/controllers/fastft.js
+++ server/controllers/fastft.js
@@ -4,13 +4,20 @@ var cacheControl = require('../utils/cache-control');
 var fetchres = require('fetchres');
 
 module.exports = function(req, res, next) {
-	fetch('http://clamo.ftdata.co.uk/api?' + encodeURI('request=[{"action":"getPost","arguments":{"id":' + req.params[0] + '}}]'), {
+	var id = req.params[0];
+
+	fetch('http://clamo.ftdata.co.uk/api?' + encodeURI('request=[{"action":"getPost","arguments":{"id":' + id + '}}]'), {
 		timeout: 3000
 	})
 		.then(fetchres.json)
 		.then(function(response) {
-			res.set(cacheControl);
-			res.redirect('/' + response[0].data.uuidv3);
+			if (response[0].status === 'ok') {
+				res.set(cacheControl);
+				res.redirect('/' + response[0].data.uuidv3);
+			} else {
+				console.error('FastFT: failed getting "%s" with message "%s"', id, response[0].title)
+				res.status(404).end();
+			}
 		})
 		.catch(next);
 };
--- tests/server/smoke-spec.js
+++ tests/server/smoke-spec.js
@@ -11,6 +11,8 @@ var $ = require('cheerio');
 var articleV1 = require('fs').readFileSync('tests/fixtures/capi1.json', { encoding: 'utf8' });
 var articleV2 = require('fs').readFileSync('tests/fixtures/capi2.json', { encoding: 'utf8' });
 var search = require('fs').readFileSync('tests/fixtures/search-for__climate-change', { encoding: 'utf8' });
+var fastFtBody = require('fs').readFileSync('tests/fixtures/fastft/rocket.json', { encoding: 'utf8' });
+var fastFtErrorBody = require('fs').readFileSync('tests/fixtures/fastft/notfound.json', { encoding: 'utf8' });
 
 var host = 'http://localhost:' + PORT;
 
@@ -85,28 +87,53 @@ describe('smoke tests for the app', function () {
 			servesGoodHTML('/d0a14962-6e56-11e4-afe5-00144feabdc0', done);
 		});
 
-		it('Should serve a fastft article', function (done) {
-			servesGoodHTML('/fastft/237332/rocket-internet-has-12-proven-losers-1st-half', done);
+	});
+
+	describe('Fast FT', function () {
+
+		it('should redirect to article', function (done) {
+			nock('http://clamo.ftdata.co.uk')
+				.get('/api?request=%5B%7B%22action%22:%22getPost%22,%22arguments%22:%7B%22id%22:237332%7D%7D%5D')
+				.reply(200, fastFtBody);
+
+			request({
+				url: host + '/fastft/237332/rocket-internet-has-12-proven-losers-1st-half',
+				followRedirect: false
+			}, function (error, response, body) {
+				expect(response.statusCode).to.equal(302);
+				expect(response.headers.location).to.equal('/cfae5a13-08a0-39d6-b36c-452d03ee44aa');
+				done();
+			});
+		});
+
+		it('should 404 if error response', function (done) {
+			nock('http://clamo.ftdata.co.uk')
+				.get('/api?request=%5B%7B%22action%22:%22getPost%22,%22arguments%22:%7B%22id%22:237332%7D%7D%5D')
+				.reply(200, fastFtErrorBody);
+
+			request({
+				url: host + '/fastft/237332/rocket-internet-has-12-proven-losers-1st-half'
+			}, function (error, response, body) {
+				expect(response.statusCode).to.equal(404);
+				done();
+			});
 		});
 
 	});
 
-	describe('tracking', function () {
+	describe('Link Tracking', function () {
 
 		beforeEach(function () {
 			mockMethode();
 		});
 
 		it('should add tracking to all article links', function (done) {
-			request.get(host + '/d0a14962-6e56-11e4-afe5-00144feabdc0', function(error, res, body) {
+			request(host + '/d0a14962-6e56-11e4-afe5-00144feabdc0', function(error, response, body) {
 				$(body).find('.article a').each(function (index, el) {
-					var link = $(el);
-					expect(link.attr('data-trackable'), 'href="' + link.attr('href') + '"').to.not.be.undefined;
+					var $link = $(el);
+					expect($link.attr('data-trackable'), 'href="' + $link.attr('href') + '"').to.not.be.undefined;
 				});
 				done();
-			}, function (err) {
-				console.log('An error has occurred', err);
-				done(err);
 			});
 		});
 
