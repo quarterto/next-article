2b2018939916b1858e0cb84d22ea0e103b5cefe5
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-24 10:02:32 +0000

    adding headline

--- server/transforms/promo-box.js
+++ server/transforms/promo-box.js
@@ -5,16 +5,20 @@ var $ = require('cheerio');
 module.exports = function(index, el) {
 	var $el = $(el);
 	var $promoBox = $('<div></div')
-		.addClass('article__promobox ng-pull-out ng-inline-element');
+		.addClass('article__promo-box ng-pull-out ng-inline-element');
 	var $title = $('<h3></h3>')
-		.addClass('article__promobox__title')
+		.addClass('article__promo-box__title')
 		.text($el.find('promo-title').text());
+	var $headline = $('<h4></h4>')
+		.addClass('article__promo-box__headline')
+		.html($el.find('promo-headline p').html().trim());
 	var image = $el.find('promo-image')
 		.html();
 	var content = $el.find('promo-intro').html();
 
 	return $promoBox
 		.append($title)
+		.append($headline)
 		.append(image)
 		.append(content);
 };
