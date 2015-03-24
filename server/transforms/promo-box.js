535e687c1d0b1a0bc9615bff8685081354f44f9a
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-24 10:21:29 +0000

    handle no headline

--- server/transforms/promo-box.js
+++ server/transforms/promo-box.js
@@ -9,16 +9,21 @@ module.exports = function(index, el) {
 	var $title = $('<h3></h3>')
 		.addClass('article__promo-box__title')
 		.text($el.find('promo-title').text());
-	var $headline = $('<h4></h4>')
-		.addClass('article__promo-box__headline')
-		.html($el.find('promo-headline p').html().trim());
 	var image = $el.find('promo-image')
 		.html();
 	var content = $el.find('promo-intro').html();
 
+	$promoBox.append($title)
+
+	if ($el.find('promo-headline').length) {
+		var $headline = $('<h4></h4>')
+			.addClass('article__promo-box__headline')
+			.html($el.find('promo-headline p').html().trim());
+
+		$promoBox.append($headline)
+	}
+
 	return $promoBox
-		.append($title)
-		.append($headline)
 		.append(image)
 		.append(content);
 };
--- tests/server/promo-box.test.js
+++ tests/server/promo-box.test.js
@@ -28,4 +28,23 @@ describe('Promo Box', function () {
 		);
 	});
 
+	it('should handle no promo-box headling', function () {
+		var $ = cheerio.load(
+			'<promo-box>' +
+				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
+				'<promo-image><ft-content data-embedded="true" type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/9ccaf9da-cde2-11e4-0f22-978e959e1c97"></ft-content></promo-image>' +
+				'<promo-intro><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></promo-intro>' +
+			'</promo-box>'
+		);
+
+		$('promo-box').replaceWith(promoBoxTransform);
+		$.html().should.equal(
+			'<div class="article__promo-box ng-pull-out ng-inline-element">' +
+				'<h3 class="article__promo-box__title">Tatomer Riesling 2012</h3>' +
+				'<ft-content data-embedded="true" type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/9ccaf9da-cde2-11e4-0f22-978e959e1c97"></ft-content>' +
+				'<p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (&#xA3;26.95, Roberson)</p>' +
+			'</div>'
+		);
+	});
+
 });
