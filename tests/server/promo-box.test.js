b470e6548323f88eb12f9a90102a13c7df082892
Matt Chadburn <matt.chadburn@ft.com>
2015-03-25 21:34:18 +0000

    Hack out broken code - test case 08ed9e04-d2fe-11e4-b7a8-00144feab7de

--- tests/server/promo-box.test.js
+++ tests/server/promo-box.test.js
@@ -21,7 +21,6 @@ describe('Promo Box', function () {
 		$.html().should.equal(
 			'<div class="article__promo-box ng-pull-out ng-inline-element">' +
 				'<h3 class="article__promo-box__title">Tatomer Riesling 2012</h3>' +
-				'<h4 class="article__promo-box__headline"><a href="http://www.ft.com/indepth/greece-debt-crisis" title="Greece debt crisis in depth - FT.com">Greece debt crisis</a></h4>' +
 				'<ft-content data-embedded="true" type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/9ccaf9da-cde2-11e4-0f22-978e959e1c97"></ft-content>' +
 				'<div class="article__promo-box__content"><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (&#xA3;26.95, Roberson)</p></div>' +
 			'</div>'
