dfbc6378d429341b280a45e4d12edcd07e2fd01b
Matthew Andrews <matt@mattandre.ws>
2015-03-31 18:07:20 +0100

    Broken FastFT posts

--- server/controllers/capi.js
+++ server/controllers/capi.js
@@ -121,7 +121,7 @@ module.exports = function(req, res, next) {
 						});
 
 					// Update the images (resize, add image captions, etc)
-					images($body, res.locals.flags)
+					return images($body, res.locals.flags)
 						.then(function ($body) {
 							res.render('layout', {
 								article: article,
--- server/transforms/byline.js
+++ server/transforms/byline.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = function(byline, articleV1) {
-	articleV1.item.metadata.authors.forEach(function (author) {
-		var name = author.term.name;
-		byline = byline.replace(
-			new RegExp('\\b(' + name + ')\\b'),
-			'<a class="article__author ng-title-link" href="/stream/authors/' + name + '" data-trackable="author">$1</a>'
-		);
-	});
+	if (articleV1 && articleV1.item && articleV1.item.metadata && articleV1.item.metadata.authors) {
+		articleV1.item.metadata.authors.forEach(function (author) {
+			var name = author.term.name;
+			byline = byline.replace(
+				new RegExp('\\b(' + name + ')\\b'),
+				'<a class="article__author ng-title-link" href="/stream/authors/' + name + '" data-trackable="author">$1</a>'
+			);
+		});
+	}
 
 	return byline;
 };
