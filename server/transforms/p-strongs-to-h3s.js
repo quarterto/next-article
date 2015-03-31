2c87d60a49e6c2bacc7a375e7a0665f536b3c5db
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-31 17:06:42 +0100

    only transform if top level

--- server/transforms/p-strongs-to-h3s.js
+++ server/transforms/p-strongs-to-h3s.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(body) {
-	return body.replace(/<p>\s*<strong>([^<]+?)<\/strong>\s*<\/p>/g, '<h3 class="ft-subhead">$1</h3>');
+var $ = require('cheerio');
+
+module.exports = function(index, el) {
+	var $el = $(el);
+
+	if ($el.html().match(/\s*<strong>([^<]+?)<\/strong>\s*/)) {
+		return '<h3 class="ft-subhead">' + $el.text() + '</h3>';
+	} else {
+		return '<p>' + $el.html() + '</p>';
+	}
 };
