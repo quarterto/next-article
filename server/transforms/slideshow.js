c37ed0c5f4aa16865aae9b23ac4c4b0dc6b1c544
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-01 14:44:50 +0100

    move slideshows outside of containing p

--- server/transforms/slideshow.js
+++ server/transforms/slideshow.js
@@ -2,7 +2,14 @@
 
 var $ = require('cheerio');
 module.exports = function(index, el) {
-	el = $(el);
-	var uuid = el.attr('href').replace(/.*([a-zA-Z0-9-]{36}).*/, '$1');
-	return '<ft-slideshow data-uuid="' + uuid + '"></ft-slideshow>';
+	var $el = $(el);
+	var uuid = $el.attr('href').replace(/.*([a-zA-Z0-9-]{36}).*/, '$1');
+	var slideshow = '<ft-slideshow data-uuid="' + uuid + '"></ft-slideshow>';
+
+	if ($el.parent('p').length) {
+		$el.parent().before(slideshow);
+		return '';
+	} else {
+		return slideshow;
+	}
 };
