166e46ced39c590bb060f16b4f3afdb154b70d2d
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-02 16:08:06 +0100

    Adding comment

--- server/transforms/slideshow.js
+++ server/transforms/slideshow.js
@@ -6,6 +6,7 @@ module.exports = function(index, el) {
 	var uuid = $el.attr('href').replace(/.*([a-zA-Z0-9-]{36}).*/, '$1');
 	var slideshow = '<ft-slideshow data-uuid="' + uuid + '"></ft-slideshow>';
 
+	// NOTE - can be removed once the slideshow is moved out of p's upstream
 	if ($el.parent('p').length) {
 		$el.parent().before(slideshow);
 		return '';
