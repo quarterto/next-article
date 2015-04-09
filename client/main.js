8ff5b08e8361b6418d727885b77696b469f263fd
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-09 16:49:25 +0100

    putting behind a switch

--- client/main.js
+++ client/main.js
@@ -57,14 +57,27 @@ setup.bootstrap(function(result) {
 	toc.init(flags);
 	oDate.init(document.querySelector('.article'));
 
-	oComments.on('widget.renderComplete', function (ev) {
-		var commentCount = ev.detail.widget.lfWidget.getCollection().attributes.numVisible;
-		var commentLink = document.createElement('a');
-		commentLink.setAttribute('href', '#comments');
-		commentLink.setAttribute('data-trackable', 'view-comments');
-		commentLink.className = 'article__actions__action article__actions__action--comments ng-meta ng-title-link';
-		commentLink.textContent = 'Comments (' + commentCount + ')';
-		document.querySelector('.article__actions').appendChild(commentLink);
-	});
-	oComments.initDomConstruct();
+	if (flags.get('articleComments').isSwitchedOn) {
+		oComments.on('widget.renderComplete', function (ev) {
+			var commentCount = ev.detail.widget.lfWidget.getCollection().attributes.numVisible;
+			var commentLink = document.createElement('a');
+			commentLink.setAttribute('href', '#comments');
+			commentLink.setAttribute('data-trackable', 'view-comments');
+			commentLink.className = 'article__actions__action article__actions__action--comments ng-meta ng-title-link';
+			commentLink.textContent = 'Comments (' + commentCount + ')';
+			document.querySelector('.article__actions').appendChild(commentLink);
+		});
+		var oCommentComponent = new oComments.Widget({
+		    elId: 'comments',
+		    title: document.title,
+		    url: document.location.href,
+		    articleId: uuid,
+		    initExtension: {
+		        initialNumVisible: 10,
+		        disableIE8Shim: true,
+		        disableThirdPartyAnalytics: true
+		    }
+		});
+		oCommentComponent.load();
+	}
 });
