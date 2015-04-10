ee57fdd5771bcd3d1cc1f4a88f7ae77374f992a9
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-10 14:55:34 +0100

    helper for media

--- client/components/video/main.js
+++ client/components/video/main.js
@@ -19,7 +19,7 @@ function brightcoveInit(el) {
 			videoEl.setAttribute('data-content-id', data.id);
 			videoEl.setAttribute(
 				'poster',
-				'//image.webservices.ft.com/v1/images/raw/' + encodeURIComponent(data.poster) + '?width=690&source=grumman&fit=scale-down'
+				'//image.webservices.ft.com/v1/images/raw/' + encodeURIComponent(data.poster) + '?width=710&source=grumman&fit=scale-down'
 			);
 			videoEl.setAttribute('controls', 'true');
 			videoEl.className = 'article__video';
