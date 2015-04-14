60fa03809ddc284cc7569c813fa872ffd1419851
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-14 14:35:16 +0100

    making tags unique

--- server/utils/tags.js
+++ server/utils/tags.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var api = require('next-ft-api-client');
 var logger = require('ft-next-logger');
 
@@ -9,7 +10,7 @@ module.exports = {
 		var articleV1Metadata = articleV1 && articleV1.item.metadata;
 		// NOTE - yoinked from
 		// https://github.com/Financial-Times/ft-api-client/blob/b95cb14b243436407fc14e1bb155e318264dfde7/lib/models/article.js#L345
-		var tagProimises = articleV1Metadata && [articleV1Metadata.primaryTheme]
+		var tagProimises = articleV1Metadata ? _(articleV1Metadata.primaryTheme)
 			.concat(
 				articleV1Metadata.people,
 				articleV1Metadata.regions,
@@ -19,10 +20,10 @@ module.exports = {
 			.filter(function (tag) {
 				return tag;
 			})
+			.uniq(function (tag) { return tag.term.id; })
 			.slice(0, 5)
 			.map(function (tag) {
 				var taxonomy = tag.term.taxonomy;
-
 				// for people or organisations, get their v2 data
 				if (flags.mentionsV2 && flags.mentionsV2.isSwitchedOn && ['people', 'organisations'].indexOf(taxonomy) > -1) {
 					return api.mapping(tag.term.id, taxonomy)
@@ -42,13 +43,12 @@ module.exports = {
 						url: '/stream/' + taxonomy + '/' + tag.term.name
 					});
 				}
-			});
+			})
+			.value() : [];
 
 		return Promise.all(tagProimises)
 			.then(function (tags) {
-				return tags.filter(function (tag) {
-					return tag;
-				})
+				return _.compact(tags);
 			});
 	}
 
