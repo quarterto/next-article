8fc1fd9da65b443232dd28ed428f1d00288569f3
Matthew Andrews <matt@mattandre.ws>
2015-04-20 12:56:55 +0100

    Simplify the ‘how useful’ component

--- client/components/how-useful/main.js
+++ client/components/how-useful/main.js
@@ -7,6 +7,13 @@ module.exports.init = function(options) {
 	var uuid = options.uuid;
 	var el = document.querySelector('.js-how-useful');
 	if (!el) return;
+
+	el.innerHTML = 'Was this article useful to you?'
+		+ '<button class="js-how-useful__yes how-useful__yes" data-trackable="yes">Yes</button>'
+		+ '<button class="js-how-useful__no how-useful__no" data-trackable="no">No</button>';
+	el.classList.add('how-useful');
+	el.setAttribute('data-trackable', 'how-useful');
+
 	var delegate = new Delegate(el);
 	delegate.on('click', '.js-how-useful__yes', processUseful.bind(this, true));
 	delegate.on('click', '.js-how-useful__no', processUseful.bind(this, false));
