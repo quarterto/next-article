87366efac2908939fe88b6c436022e433d808b7e
Matthew Andrews <matt@mattandre.ws>
2015-05-19 14:06:20 +0100

    Push external images through image service

--- server/transforms/external-img.js
+++ server/transforms/external-img.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var $ = require('cheerio');
+var resize = require('../utils/resize');
 
 module.exports = function(index, el) {
+	var $el = $(el).clone();
+	$el.removeAttr('height');
+	$el.attr('src', resize($el.attr('src')));
 	return $('<figure></figure>')
 		.addClass('article__image-wrapper article__inline-image ng-inline-element ng-pull-out')
-		.append($(el).clone().addClass('article__image'));
+		.append($el.addClass('article__image'));
 };
