b0c3e4dcf6ab3c2cbd2f602c564b9b149806d308
Matthew Andrews <matt@mattandre.ws>
2015-05-19 14:18:49 +0100

    Images on articles can never be bigger than 710

--- server/transforms/external-img.js
+++ server/transforms/external-img.js
@@ -6,7 +6,7 @@ var resize = require('../utils/resize');
 module.exports = function(index, el) {
 	var $el = $(el).clone();
 	$el.removeAttr('height');
-	$el.attr('src', resize($el.attr('src')));
+	$el.attr('src', resize($el.attr('src'), { width: 710 }));
 	return $('<figure></figure>')
 		.addClass('article__image-wrapper article__inline-image ng-inline-element ng-pull-out')
 		.append($el.addClass('article__image'));
