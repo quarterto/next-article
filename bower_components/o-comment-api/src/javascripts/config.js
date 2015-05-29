d092e6ed823e211d9ac4b92630d1211d51c90ab2
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-05-29 16:31:30 +0100

    ignore all of o-comments

--- bower_components/o-comment-api/src/javascripts/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var EnvConfig = require('o-comment-utilities').EnvConfig;
-
-/**
- * Isolated copy of the configuration.
- * @type {js-env-config}
- */
-module.exports = new EnvConfig();
--- bower_components/o-comments/src/javascripts/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var EnvConfig = require('o-comment-utilities').EnvConfig;
-
-module.exports = new EnvConfig();
