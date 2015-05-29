d1eb1d2fa2b9a90b8a65d65aeef91366c8666a1f
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-05-29 16:04:01 +0100

    use n-video

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ var myFtClient = require('next-myft-client');
 
 var setup = require('next-js-setup');
 var header = require('next-header');
-var video = require('next-video');
+var nVideo = require('n-video');
 
 var slideshow = require('./components/slideshow/main');
 var moreOn = require('./components/more-on/main');
@@ -36,13 +36,12 @@ setup.bootstrap(function(result) {
 		moreOn.init(flags);
 	}
 
-	require('./components/video/main').init()
-		.then(function () {
-			// so far next-video only adds analytics
-			// eventually most of the brightcove bit of video component will go in here
-			video.init();
+	nVideo.init()
+		.then(function(videos) {
+			videos.forEach(function (video) {
+				video.el.className = 'article__video ng-media';
+			});
 		});
-
 	toc.init(flags);
 	comments.init(uuid, flags);
 	oDate.init(document.querySelector('.article'));
