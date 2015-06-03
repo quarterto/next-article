d60831df20acafd7f9b4fe49dd013524e22cb765
Rhys Evans <rhys.evans@ft.com>
2015-06-03 15:33:05 +0100

    various refinements to styles

--- server/utils/exclude-primary-theme.js
+++ server/utils/exclude-primary-theme.js
@@ -2,7 +2,7 @@
 
 module.exports = function (article) {
 
-	var exclude = article.item.metadata.primaryTheme.term.id;
+	var exclude = article.item.metadata.primaryTheme && article.item.metadata.primaryTheme.term.id;
 
 	return function (topic) {
 		return topic.term.id !== exclude;
