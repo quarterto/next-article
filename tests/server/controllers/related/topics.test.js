54dd5e1c1ad74966ed6046b5c05acbef6390838f
Rhys Evans <rhys.evans@ft.com>
2015-06-04 10:49:17 +0100

    tests

--- tests/server/controllers/related/topics.test.js
+++ tests/server/controllers/related/topics.test.js
@@ -8,18 +8,14 @@ var helpers = require('../../helpers');
 
 module.exports = function() {
 
-	describe('Related - People', function() {
+	describe('Related - Topics', function() {
 
 		it('should be successful ', function() {
 			nock('https://ft-elastic-search.com')
 				.post('/v1_api_v2/item/_mget')
 				.reply(200, require('../../../fixtures/capi-v1-elastic-search.json'));
-			nock('https://next-v1tov2-mapping-dev.herokuapp.com')
-				.filteringPath(/^\/concordance_mapping_v1tov2\/people\/.*$/, '/concordance_mapping_v1tov2/people/XXX')
-				.get('/concordance_mapping_v1tov2/people/XXX')
-				.reply(200, require('../../../fixtures/mapping.json'));
 
-			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/people')
+			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/topics')
 				.then(function (response) {
 					response.status.should.equal(200);
 					return response.text().then(function (text) {
@@ -34,18 +30,18 @@ module.exports = function() {
 				.times(4)
 				.reply(404);
 
-			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/people')
+			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/topics')
 				.then(function (response) {
 					response.status.should.equal(404);
 				});
 		});
 
-		it('should return 200 and empty response if no related people', function () {
+		it('should return 200 and empty response if no related topics', function () {
 			nock('https://ft-elastic-search.com')
 				.post('/v1_api_v2/item/_mget')
 				.reply(200, require('../../../fixtures/capi-v1-elastic-search-no-meta.json'));
 
-			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/people')
+			return fetch(helpers.host + '/f2b13800-c70c-11e4-8e1f-00144feab7de/topics')
 				.then(function (response) {
 					response.status.should.equal(200);
 					return response.text().then(function (text) {
