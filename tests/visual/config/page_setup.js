70e4ddaacbc2b510a92fea15d638ab77f8bdb349
Matthew Andrews <matt@mattandre.ws>
2015-06-12 14:49:54 +0100

    Start simpler

--- tests/visual/config/page_setup.js
+++ tests/visual/config/page_setup.js
@@ -10,45 +10,9 @@ module.exports = {
 		"name": "article_with_image",
 		"path": "ccf7af08-e904-11e4-a71a-00144feab7de",
 		"elements": {
-			"header_logo": {
+			"header": {
 				"name":"header_logo",
-				"css":'.next-header__logo--ft',
-				"widths":[
-					500,
-					1400
-				]
-			},
-			"stand-first": {
-				"name":"stand-first",
-				"css":'.article__stand-first',
-				"widths":[
-					350,
-					600,
-					900,
-					1000,
-					1400
-				]
-			},
-			"metadata": {
-				"name":"metadata",
-				"css":'.article__meta',
-				"widths":[
-					350,
-					600,
-					900,
-					1000,
-					1400
-				]
-			}
-		}
-	},
-	"article_with_video":{
-		"name": "article_with_video",
-		"path":"05faa536-d3ab-11e4-a9d3-00144feab7de",
-		"elements":{
-			"header_logo": {
-				"name":"header_logo",
-				"css":'.next-header__logo--ft',
+				"css":'.article__header',
 				"widths":[
 					500,
 					1400
