aee28c31a76a58e80b1bb3c65fdc35ba8fcc2ca6
Matthew Andrews <matt@mattandre.ws>
2015-06-13 01:12:23 +0100

    More tidying up of repetition of folders

--- tests/visual/visual-runner.js
+++ tests/visual/visual-runner.js
@@ -21,6 +21,7 @@ var page_data = require('./config');
 var screenshots;
 var failures;
 
+var LOCAL_PREFIX = "tests/visual/screenshots";
 var AWS_DEST_PREFIX = "image_diffs/" + normalizeName(packageJson.name, { version: false }) + "/" + moment().format('YYYY-MM-DD') + "/" + moment().format('HH:mm') + "-" + process.env.TRAVIS_BUILD_NUMBER + "/";
 var AWS_SHOTS_INDEX = "https://s3-eu-west-1.amazonaws.com/ft-next-qa/" + AWS_DEST_PREFIX + "/successes/index.html";
 var AWS_FAILS_INDEX = "https://s3-eu-west-1.amazonaws.com/ft-next-qa/" + AWS_DEST_PREFIX + "/failures/index.html";
@@ -68,31 +69,27 @@ Promise.all(imageDiffPromises)
 		var promises = [];
 		console.log("\n\nCasperJS output: \n\n" + result);
 
-		if (fs.existsSync("tests/visual/screenshots/successes")) {
+		if (fs.existsSync(LOCAL_PREFIX + "successes")) {
 
 			// find all screenshots and build an html page to display them
-			screenshots = fs.readdirSync("tests/visual/screenshots/successes");
+			screenshots = fs.readdirSync(LOCAL_PREFIX + "successes");
 			var screenshotspage = buildIndexPage(screenshots);
-			promises.push(writeFile("tests/visual/screenshots/successes/index.html", screenshotspage));
+			promises.push(writeFile(LOCAL_PREFIX + "successes/index.html", screenshotspage));
 
 			// add path to screenshots
-			screenshots = screenshots.map(function(screenshot) {
-				return "tests/visual/screenshots/successes/" + screenshot;
-			});
+			screenshots = screenshots.map(function(screenshot) { return "successes/" + screenshot; });
 			console.log("Screenshots located at " + AWS_SHOTS_INDEX);
 		} else {
 			console.log("No screenshots here");
 		}
 
-		if (fs.existsSync("tests/visual/screenshots/failures")) {
-			failures = fs.readdirSync("tests/visual/screenshots/failures");
+		if (fs.existsSync(LOCAL_PREFIX + "failures")) {
+			failures = fs.readdirSync(LOCAL_PREFIX + "failures");
 			var failurespage = buildIndexPage(failures);
-			promises.push(writeFile("tests/visual/screenshots/failures/index.html", failurespage));
+			promises.push(writeFile(LOCAL_PREFIX + "failures/index.html", failurespage));
 
 			// add path to failures
-			failures = failures.map(function(failure) {
-				return "tests/visual/screenshots/failures/" + failure;
-			});
+			failures = failures.map(function(failure) { return "failures/" + failure; });
 			console.log("Failure screenshots located at " + AWS_FAILS_INDEX);
 		} else {
 			console.log("No failures found");
@@ -101,17 +98,17 @@ Promise.all(imageDiffPromises)
 		return Promise.all(promises);
 	})
 	.then(function() {
-		var files = ["tests/visual/screenshots/successes/index.html"]
+		var files = ["successes/index.html"]
 			.concat(screenshots);
 
-		if (fs.existsSync("tests/visual/screenshots/failures")) {
+		if (fs.existsSync(LOCAL_PREFIX + "failures")) {
 			files = files
 				.concat(failures)
-				.concat(["tests/visual/screenshots/failures/index.html"]);
+				.concat(["failures/index.html"]);
 		}
 
 		return deployStatic({
-			files: files,
+			files: files.map(function(file) { return LOCAL_PREFIX + "" + file; }),
 			destination: AWS_DEST_PREFIX,
 			region: 'eu-west-1',
 			bucket: 'ft-next-qa',
@@ -130,9 +127,7 @@ Promise.all(imageDiffPromises)
 					user: repoSlug[0],
 					repo: repoSlug[1],
 					number: pullRequest,
-					body: "Image diffs found between branch and production" +
-					"\nSee" +
-					"\n\n" + AWS_FAILS_INDEX
+					body: "[Image diffs found between branch and production](" + AWS_FAILS_INDEX + ")"
 				});
 		} else {
 			console.log("No comments to make to Pull Request");
