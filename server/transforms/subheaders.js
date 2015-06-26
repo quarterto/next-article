dd5537eb324b46ad959ff8ecd80916c5b704789d
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-06-26 10:35:06 +0100

    use crossheads

--- server/transforms/subheaders.js
+++ server/transforms/subheaders.js
@@ -3,27 +3,18 @@
 var cheerio = require('cheerio');
 
 module.exports = function ($, flags) {
-	if (flags.articleCrossheads) {
-		$('.ft-subhead').each(function (index, subhead) {
-			var $subhead = cheerio(subhead);
-			var $newSubhead = cheerio('<h2 class="article__subhead">' + $subhead.text() + '</h2');
-			var childEl = $subhead.children().get(0);
-			if (childEl && childEl.tagName === 'strong') {
-				$newSubhead.attr('id', 'crosshead-' + (index + 1));
-				$newSubhead.addClass('article__subhead--crosshead ng-pull-out');
-			} else {
-				$newSubhead.addClass('article__subhead--standard');
-			}
-			$subhead.replaceWith($newSubhead);
-		});
-	} else {
-		$('.ft-subhead')
-			.replaceWith(function(index, subhead) {
-				return '<h2 class="article__subhead article__subhead--crosshead ng-pull-out" id="crosshead-' + (index + 1) + '">' +
-					cheerio(subhead).text() +
-				'</h2>';
-			});
-	}
+	$('.ft-subhead').each(function (index, subhead) {
+		var $subhead = cheerio(subhead);
+		var $newSubhead = cheerio('<h2 class="article__subhead">' + $subhead.text() + '</h2');
+		var childEl = $subhead.children().get(0);
+		if (childEl && childEl.tagName === 'strong') {
+			$newSubhead.attr('id', 'crosshead-' + (index + 1));
+			$newSubhead.addClass('article__subhead--crosshead ng-pull-out');
+		} else {
+			$newSubhead.addClass('article__subhead--standard');
+		}
+		$subhead.replaceWith($newSubhead);
+	});
 
 	return $;
 };
--- test/server/transforms/subheaders.test.js
+++ test/server/transforms/subheaders.test.js
@@ -2,38 +2,27 @@
 'use strict';
 
 var cheerio = require('cheerio');
-var expect = require('chai').expect;
+require('chai').should();
 var subheadersTransform = require('../../../server/transforms/subheaders');
 
 describe('Subheaders', function () {
 
-	it('should update subheaders', function() {
-		var $ = cheerio.load('<h3 class="ft-subhead">The new big earners</h3>');
-		$ - subheadersTransform($, {});
-
-		expect($.html()).to.equal(
-			'<h2 class="article__subhead article__subhead--crosshead ng-pull-out" id="crosshead-1">' +
-				'The new big earners' +
-			'</h2>'
-		);
-	});
-
-	it('should create subheaders (if flag is on)', function() {
+	it('should create subheaders', function() {
 		var $ = cheerio.load('<h3 class="ft-subhead">The new big earners</h3>');
 		$ - subheadersTransform($, { articleCrossheads: true });
 
-		expect($.html()).to.equal(
+		$.html().should.equal(
 			'<h2 class="article__subhead article__subhead--standard">' +
 				'The new big earners' +
 			'</h2>'
 		);
 	});
 
-	it('should create crossheads (if flag is on)', function() {
+	it('should create crossheads, if subhead contains a strong tag', function() {
 		var $ = cheerio.load('<h3 class="ft-subhead"><strong>The new big earners</strong></h3>');
 		$ - subheadersTransform($, { articleCrossheads: true });
 
-		expect($.html()).to.equal(
+		$.html().should.equal(
 			'<h2 class="article__subhead article__subhead--crosshead ng-pull-out" id="crosshead-1">' +
 				'The new big earners' +
 			'</h2>'
