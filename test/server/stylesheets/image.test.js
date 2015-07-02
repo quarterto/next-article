cd83173162a67900bde97bb922e126c4ed7b321a
Matthew Andrews <matt@mattandre.ws>
2015-07-02 11:12:28 +0100

    address feedback from @i-like-robots

--- test/server/stylesheets/image.test.js
+++ test/server/stylesheets/image.test.js
@@ -32,6 +32,23 @@ describe('Images', function () {
 			});
 	});
 
+	it('should move images out of containing <p> if they\'re the only thing in it (ignoring whitespace)', function() {
+		return transform(
+				'<body>' +
+					'<p>    ' +
+						'<ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/ab3c20e8-15fe-11e5-2032-978e959e1689" data-embedded="true"></ft-content>' +
+					'			</p>' +
+				'</body>'
+			)
+			.then(function (transformedXml) {
+				transformedXml.should.equal(
+					'<body>' +
+						'<ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/ab3c20e8-15fe-11e5-2032-978e959e1689" data-embedded="true"/>' +
+					'</body>'
+				);
+			});
+	});
+
 	it('should not move images out of containing <p> if they\'re not the only thing in it', function() {
 		return transform(
 				'<body>' +
