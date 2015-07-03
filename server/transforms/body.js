623e8c4f49287a6f679c8d2f192464e4ce03c4cf
Matthew Andrews <matt@mattandre.ws>
2015-07-03 16:46:12 +0100

    Switch interactives over to a next https one

--- server/transforms/body.js
+++ server/transforms/body.js
@@ -29,6 +29,8 @@ module.exports = function(body, flags) {
 	body = replaceEllipses(body);
 	body = replaceHrs(body);
 	body = body.replace(/<\/a>\s+([,;.:])/mg, '</a>$1');
+	body = body.replace(/http:\/\/www\.ft\.com\/ig\//g, 'https://ig-next.ft.com/');
+	body = body.replace(/http:\/\/ig\.ft\.com\//g, 'https://ig-next.ft.com/');
 
 	var $ = transform(cheerio.load(body, { xmlMode: true }), flags)
 		// other transforms
