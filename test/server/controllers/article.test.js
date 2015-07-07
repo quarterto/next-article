94a8ed96da364feabaee208fdca5c851c1e42cea
Matt Chadburn <matt.chadburn@ft.com>
2015-07-07 12:28:46 +0100

    Fix test (with mitm)

--- test/server/controllers/article.test.js
+++ test/server/controllers/article.test.js
@@ -1,11 +1,11 @@
-/*global it, describe*/
+/*global beforeEach, afterEach, it, describe*/
 'use strict';
 
 var expect = require('chai').expect;
 require('chai').should();
-var nock = require('nock');
 var request = require('request');
 var $ = require('cheerio');
+var mitm = require('mitm');
 
 var helpers = require('../helpers');
 var articleV2 = require('../../fixtures/capi-v2.json');
@@ -35,23 +35,43 @@ module.exports = function () {
 			});
 		});
 
-		it.skip('should work not using elastic search', function (done) {
-			nock('http://api.ft.com')
-				.get('/content/items/v1/0369dd4e-8513-11e1-2a93-978e959e1fd3?feature.blogposts=on&bodyFormat=structured')
-				.reply(200, require('../../fixtures/capi-v1.json'));
-			nock('http://api.ft.com')
-				.get('/enrichedcontent/0369dd4e-8513-11e1-2a93-978e959e1fd3')
-				.reply(200, articleV2);
+		describe('elastic search', function () {
 
-			request({
-				url: helpers.host + '/0369dd4e-8513-11e1-2a93-978e959e1fd3',
-				headers: { Cookie: 'next-flags=elasticSearchItemGet:off' }
-			}, function (error, response, body) {
-				response.statusCode.should.equal(200);
-				done();
+			beforeEach(function() {
+				this.mitm = mitm();
+			});
+
+			afterEach(function() {
+				this.mitm.disable();
+			});
+
+			it('should work not using elastic search', function (done) {
+
+				this.mitm.on('request', function(req, res) {
+					switch (req.url) {
+						case '/0369dd4e-8513-11e1-2a93-978e959e1fd3':
+							res.statusCode = 200;
+							res.end();							
+							break;
+						case '/enrichedcontent/0369dd4e-8513-11e1-2a93-978e959e1fd3':
+							res.statusCode = 200;
+							res.end(articleV2);
+							break;
+						default:
+							throw new Error('request not understood'); 
+					}
+				});
+				
+				request({
+					url: helpers.host + '/0369dd4e-8513-11e1-2a93-978e959e1fd3',
+					headers: { Cookie: 'next-flags=elasticSearchItemGet:off' }
+				}, function (error, response, body) {
+					response.statusCode.should.equal(200);
+					done();
+				});
+	
 			});
 		});
-
 	});
 
 };
