cc5df4d2fbacd041e08afe9897372b28913446f0
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-07-10 11:33:55 +0100

    Add libxslt compiled binaries

---
+++ .heroku/vendor/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/app/.heroku/vendor/lib"
+XSLT_LIBS="-lxslt  -lxml2 -lm -lrt"
+XSLT_INCLUDEDIR="-I/app/.heroku/vendor/include"
+MODULE_VERSION="xslt-1.1.28"
