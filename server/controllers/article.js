929bff9cd26051c8824835c00a82d4a442066ad0
Adam Silver <adambsilver@gmail.com>
2015-07-13 11:15:16 +0100

    remove bullets last item

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -158,7 +158,7 @@ module.exports = function(req, res, next) {
 							viewModel.trialSimpleBarrier = res.locals.barrier.trialSimple;
 						}
 
-						if(res.locals.barrier.trialGrid) {
+						if(res.locals.barrier.trialGrid == 123) {
 
 							viewModel.trialGridBarrier = res.locals.barrier.trialGrid;
 
@@ -172,8 +172,8 @@ module.exports = function(req, res, next) {
 							viewModel.barrierOverlay = {};
 						}
 
-						if(res.locals.barrier.subscriptionGrid) {
-							viewModel.subscriptionGridBarrier = res.locals.barrier.subscriptionGrid;
+						if(res.locals.barrier.trialGrid) {
+							viewModel.subscriptionGridBarrier = res.locals.barrier.trialGrid;
 							viewModel.subscriptionGridBarrier.articleTitle = viewModel.title;
 							viewModel.barrierOverlay = {};
 						}
