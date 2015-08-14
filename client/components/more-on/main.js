18eb46a66c828d289f8c8fdb7268ab3d7cba5e88
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-08-14 10:54:38 +0100

    don't remove topic if fails

--- client/components/more-on/main.js
+++ client/components/more-on/main.js
@@ -5,42 +5,45 @@ var oDate = require('o-date');
 var myFtUi = require('next-myft-ui');
 
 // Sort of like Promise.all but will be called whether they fail or succeed
-function allSettled(promises) {
+var allSettled = promises => {
 	var resolveWhenSettled = function(promise) {
-		return new Promise(function(res) {
-			promise.then(res, function() {
-				res();
-			});
+		return new Promise(res => {
+			promise.then(res, () => res());
 		});
 	};
 	return Promise.all(promises.map(resolveWhenSettled));
-}
-
-var $ = function(selector) {
-	return [].slice.call(document.querySelectorAll(selector));
 };
 
-var createPromise = function (el, url, renderer) {
+var $ = selector => [].slice.call(document.querySelectorAll(selector));
+
+var createPromise = (el, url, options = {}) => {
+	// default options
+	var opts = {
+		renderer: null,
+		removeOnFail: true
+	};
+	Object.assign(opts, options);
 	return fetch(url, { credentials: 'same-origin' })
 		.then(fetchres.text)
-		.then(function(resp) {
+		.then(resp => {
 			if (!resp) {
 				throw new Error('No response');
 			}
-			if (renderer) {
-				renderer(el, resp);
+			if (opts.renderer) {
+				opts.renderer(el, resp);
 			} else {
 				el.innerHTML = resp;
 			}
 			oDate.init(el);
 		})
 		.catch(function() {
-			el.parentNode.removeChild(el);
+			if (opts.removeOnFail) {
+				el.parentNode.removeChild(el);
+			}
 		});
 };
 
-module.exports.init = function(flags) {
-	var fetchPromises = [];
+module.exports.init = flags => {
 	var article = document.querySelector('.article');
 	var articleId = article.getAttribute('data-content-id');
 	var articleSources = article.getAttribute('data-content-sources');
@@ -51,29 +54,32 @@ module.exports.init = function(flags) {
 		return;
 	}
 
-	$('.js-more-on-inline').forEach(function(el) {
-		fetchPromises.push(createPromise(el, '/article/' + articleId + '/story-package?count=1&view=inline'));
-	});
-	$('.js-more-on').forEach(function(el) {
-		fetchPromises.push(createPromise(el, '/article/' + articleId + '/story-package?count=4'));
-	});
-	$('.js-more-on-topic').forEach(function(el) {
-		fetchPromises.push(createPromise(el, '/article/' + articleId + '/more-on?metadata-fields=' + el.getAttribute('data-metadata-fields').replace(' ', ',') + '&count=4', function (el, resp) {
-			var brandEl = el.querySelector('.n-topic[data-taxonomy="brand"]');
-			if (brandEl) {
-				brandEl.insertAdjacentHTML('afterend', resp);
-			} else {
-				el.innerHTML = resp;
-			}
-		}));
-	});
-	$('.js-related').forEach(function(el) {
-		fetchPromises.push(createPromise(el, '/article/' + articleId + '/' + el.getAttribute('data-taxonomy')));
-	});
-	$('.js-special-report').forEach(el => fetchPromises.push(createPromise(el, '/article/' + articleId + '/special-report')));
+	var fetchPromises = [].concat(
+		$('.js-more-on-inline').map(el => createPromise(el, '/article/' + articleId + '/story-package?count=1&view=inline')),
+		$('.js-more-on').map(el => createPromise(el, '/article/' + articleId + '/story-package?count=4')),
+		$('.js-more-on-topic').map(el =>
+			createPromise(
+				el,
+				'/article/' + articleId + '/more-on?metadata-fields=' + el.getAttribute('data-metadata-fields').replace(' ', ',') + '&count=4',
+				{
+					renderer: (el, resp) => {
+						var brandEl = el.querySelector('.n-topic[data-taxonomy="brand"]');
+						if (brandEl) {
+							brandEl.insertAdjacentHTML('afterend', resp);
+						} else {
+							el.innerHTML = resp;
+						}
+					},
+					removeOnFail: false
+				}
+			)
+		),
+		$('.js-related').map(el => createPromise(el, '/article/' + articleId + '/' + el.getAttribute('data-taxonomy'))),
+		$('.js-special-report').map(el => createPromise(el, '/article/' + articleId + '/special-report'))
+	);
 
 	return allSettled(fetchPromises)
-		.then(function() {
+		.then(() => {
 			var moreOnContainer = document.querySelector('.article__more-on');
 			myFtUi.updateUi(moreOnContainer);
 		});
