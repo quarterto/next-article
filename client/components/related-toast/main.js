e4bb6c8639f301fc51dc6d65df19561dd2a04a92
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-08-26 14:11:21 +0100

    link to correct place

--- client/components/related-toast/main.js
+++ client/components/related-toast/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
+var throttle = require('../../libs/throttle');
+
 var appendToast = (article, topic) => {
-    var toastEl = document.createElement('div');
-    toastEl.className = 'article__toast article__toast--hide';
-    toastEl.innerHTML =
+    var toastTmplEl = document.createElement('div');
+    toastTmplEl.classList.add('article__toast');
+    toastTmplEl.innerHTML =
         `<div class="article__toast__inner o-grid-row">
             <div class="article__toast__header o-grid-remove-gutters" data-o-grid-colspan="1">
                 <h2>Read next</h2>
             </div>
              <div class="article__toast__link o-grid-remove-gutters" data-o-grid-colspan="3">
-                <a href="" class="article__toast__to-related" data-trackable="to-related">See more</a>
+                <a href="#related" class="article__toast__to-related js-to-related" data-trackable="to-related">See more</a>
              </div>
          </div>`;
     var articleEl = document.createElement('div');
@@ -19,16 +21,56 @@ var appendToast = (article, topic) => {
             <h3>${topic}</h3>
             <p>${article.title}</p>
         </a>`;
-    toastEl.querySelector('.article__toast__inner')
-        .insertBefore(articleEl, toastEl.querySelector('.article__toast__link'));
-    document.body.appendChild(toastEl);
-    console.log(toastEl);
-    return toastEl;
+    toastTmplEl.querySelector('.article__toast__inner')
+        .insertBefore(articleEl, toastTmplEl.querySelector('.article__toast__link'));
+
+    // fixed toast
+    var fixedToastEl = toastTmplEl.cloneNode(true);
+    fixedToastEl.classList.add('article__toast--hide');
+    fixedToastEl.classList.add('article__toast--fixed');
+    document.body.appendChild(fixedToastEl);
+
+    // in page toast
+    var inPageToastEl = toastTmplEl.cloneNode(true);
+    inPageToastEl.id = 'related';
+    inPageToastEl.classList.add('article__toast--in-page');
+    var moreOnEl = document.querySelector('.article-share').nextSibling;
+    moreOnEl.parentNode.insertBefore(inPageToastEl, moreOnEl);
+    return fixedToastEl;
+};
+
+
+var addToRelatedHandler = toastEl => {
+    const duration = 1000;
+    // https://gist.github.com/gre/1650294
+    var easeFunction = t => 1 - (--t) * t * t * t;
+    var handler = ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        var scrollFrom = document.body.scrollTop;
+        var scrollDist = document.querySelector(ev.target.getAttribute('href')).getBoundingClientRect().top;
+        var startTime = new Date();
+        var interval = window.setInterval(() => {
+            var elapsed = new Date() - startTime;
+            document.body.scrollTop = scrollFrom + (easeFunction(elapsed / duration) * scrollDist);
+        }, 15);
+
+        window.setTimeout(() => {
+            window.clearInterval(interval);
+            window.location.hash = ev.target.getAttribute('href');
+        }, duration);
+    };
+
+    toastEl.querySelector('.js-to-related').addEventListener('click', handler);
 };
 
 module.exports = {
 
     init: flags => {
+        if (!flags.get('articleRelatedToast')) {
+            return;
+        }
         // get related articles
         var articleUuid = document.querySelector('#site-content').getAttribute('data-content-id');
         fetch(`/article/${articleUuid}/more-on?metadata-fields=primaryTheme&count=1`)
@@ -39,34 +81,40 @@ module.exports = {
                 return result.text();
             })
             .then(relatedHtml => {
-                console.log('1');
                 var relatedEl = document.createElement('div');
-                console.log('2');
                 relatedEl.innerHTML = relatedHtml;
-                console.log('3');
                 var topic = relatedEl.querySelector('.n-topic__name').textContent;
-                console.log('4');
                 var articleEl = relatedEl.querySelector('.more-on__article');
-                console.log('5');
                 var article = {
                     title: articleEl.querySelector('.n-card__headline').textContent,
                     link: articleEl.querySelector('.n-card__headline__link').getAttribute('href')
                 };
                 // append
-                console.log('FOO');
                 var toastEl = appendToast(article, topic);
+                // to related handler
+                addToRelatedHandler(toastEl);
                 // get height of article
                 var articleBodyEl = document.querySelector('.article__body');
                 var articleBodyHalfwayPoint = articleBodyEl.offsetHeight / 2;
-                window.addEventListener('scroll', ev => {
+                var seen = false;
+                var throttledHandler = ev => {
                     // display when half way down article
                     var pos = articleBodyEl.getBoundingClientRect().bottom - window.innerHeight;
-                    if (pos < articleBodyHalfwayPoint && pos > 0) {
+                    if (pos < articleBodyHalfwayPoint || seen) {
+                        seen = true;
                         toastEl.classList.remove('article__toast--hide');
                     } else {
                         toastEl.classList.add('article__toast--hide');
                     }
-                });
+                    var toastPos = Math.max(window.innerHeight - document.querySelector('.article__toast--in-page').getBoundingClientRect().bottom, 0);
+                    toastEl.style.bottom = `${toastPos}px`;
+                    if (toastPos > 0) {
+                        toastEl.classList.add('article__toast--fixed-in-page');
+                    } else {
+                        toastEl.classList.remove('article__toast--fixed-in-page');
+                    }
+                };
+                window.addEventListener('scroll', throttledHandler);
             })
             .catch(console.log);
     }
