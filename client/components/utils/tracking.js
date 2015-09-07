8720a0e8dca824cab8cda5157cc9d2cefb6c13c3
Tom Parker <tom.p.parker@googlemail.com>
2015-09-07 13:05:13 +0100

    Move event to use o-tracking

--- client/components/utils/tracking.js
+++ client/components/utils/tracking.js
@@ -1,15 +1,20 @@
 'use strict';
 
+var dispatchCustomEvent = function (name, data) {
+	var rootEl = document.body;
+	var event = (function(){
+		try{
+			return new CustomEvent(name, {bubbles:true, cancelable:true, detail:data});
+		}catch(e){
+			return CustomEvent.initCustomEvent(name, true, true, data);
+		}
+	}());
+
+	rootEl.dispatchEvent(event);
+};
+
 var sendTrackingEvent = function(data) {
-	fetch('https://spoor-api.ft.com/ingest', {
-		method: 'post',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'Content-Length': new Buffer(JSON.stringify(data)).length
-		},
-		body: JSON.stringify(data)
-	});
+	dispatchCustomEvent('oTracking.event', data);
 };
 
 module.exports = sendTrackingEvent;
