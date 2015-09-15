b4dc9c2fdc290c3e7dae1a1c10c2215032e2a122
Imran Sulemanji <imranolas@gmail.com>
2015-09-15 10:26:24 +0100

    Add endpoint to render suggestedReads/articles given a post body with ids

---
+++ server/controllers/articles.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var api = require('next-ft-api-client');
+
+module.exports = function(req, res, next) {
+	console.log(req.body);
+	api.contentLegacy({
+		uuid: req.body.ids,
+		useElasticSearch: res.locals.flags.elasticSearchItemGet,
+		type: 'Article'
+	})
+	.then(function(items) {
+		res.format({
+			'application/json': function() {
+				res.send(items);
+			},
+			default: function() {
+				res.render('partials/related/suggested-reads', {items: items});
+			}
+		});
+	})
+	.catch(next);
+};
