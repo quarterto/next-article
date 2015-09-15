3c260a75b97ce45f2967e45b073db1cd74da2a9c
Imran Sulemanji <imranolas@gmail.com>
2015-09-15 10:27:10 +0100

    Add client side rendering of suggested	reads

---
+++ client/components/suggested-reads/main.js
@@ -0,0 +1,31 @@
+'use strict';
+var fetchres = require('fetchres')
+
+module.exports = {
+	init: function(flags) {
+		var el = document.querySelector('.js-suggested-reads');
+		var data = document.querySelector('#dehydrated-data');
+
+		try {
+			data = JSON.parse(data.text);
+		} catch(err) {
+			return undefined;
+		}
+
+		fetch('/articles', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/text'
+			},
+			body: JSON.stringify({
+				ids: data.suggestedReads.ids
+			})
+		})
+		.then(fetchres.text)
+		.then(html => {
+			el.innerHTML = html;
+		})
+		.catch(console.error);
+	}
+};
