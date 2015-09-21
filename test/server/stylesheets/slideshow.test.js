82bf20c72f8c017d0dec27037d0849a322c652d0
andygnewman <andrew.newman@ft.com>
2015-09-21 12:40:08 +0100

    get transforms working on para with slideshow

--- test/server/stylesheets/slideshow.test.js
+++ test/server/stylesheets/slideshow.test.js
@@ -110,8 +110,10 @@ describe('Slideshow', function () {
 		return transform(
 				'<body>' +
 					'<p>' +
-						'<a href="http://www.ft.com/cms/s/0/f3970f88-0475-11df-8603-00144feabdc0.html#slide0"></a>' +
-						'Another <a href="/home">link</a> within the text' +
+						'<a data-asset-type="slideshow" data-embedded="true" href="http://www.ft.com/cms/s/4fd9b5ca-5cea-11e5-9846-de406ccb37f2.html#slide0"></a>' +
+						'Abraham Lincoln and Stephen Douglas famously held three-hour debates on the eve of the US civil war. Duration was the only thing the second 2016 ' +
+						'<a href="http://blogs.ft.com/the-world/liveblogs/2015-09-15/" title="As it happened: Republican presidential debate - FTcom">Republican debate</a> ' +
+						'had in common with those legendary exchanges.' +
 					'</p>' +
 				'</body>',
 				{
@@ -121,8 +123,11 @@ describe('Slideshow', function () {
 			.then(function (transformedXml) {
 				transformedXml.should.equal(
 					'<body>' +
-						'<ft-slideshow data-uuid="f3970f88-0475-11df-8603-00144feabdc0"></ft-slideshow>' +
-						'<p>Another <a href="/home">link</a> within the text</p>' +
+						'<ft-slideshow data-uuid="4fd9b5ca-5cea-11e5-9846-de406ccb37f2"></ft-slideshow>' +
+						'<p>Abraham Lincoln and Stephen Douglas famously held three-hour debates on the eve of the US civil war. Duration was the only thing the second 2016 ' +
+						'<a href="http://blogs.ft.com/the-world/liveblogs/2015-09-15/" data-trackable="link" class="article__body__link">Republican debate</a> ' +
+						'had in common with those legendary exchanges.' +
+					'</p>' +
 					'</body>\n'
 				);
 			});
