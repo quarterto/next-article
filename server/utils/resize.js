6f196536daccce978cb5072cc19b7c49ea8b8c57
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-09-28 12:29:47 +0100

    Remove merge module, use Object.assign

--- server/utils/resize.js
+++ server/utils/resize.js
@@ -1,7 +1,6 @@
 // TODO: Combine with the equivalent in ft-next-express
 'use strict';
 
-var merge = require('merge');
 var querystring = require('querystring');
 
 module.exports = function(url, options) {
@@ -9,7 +8,7 @@ module.exports = function(url, options) {
 		source: 'next',
 		fit: 'scale-down'
 	};
-	var query = querystring.stringify(merge(defaultOptions, options));
+	var query = querystring.stringify(Object.assign({}, defaultOptions, options));
 
 	return 'https://next-geebee.ft.com/image/v1/images/raw/' + encodeURIComponent(url) + '?' + query;
 };
