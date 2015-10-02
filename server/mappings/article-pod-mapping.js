a5740b2b97ce53b1803f327a95c866ee445830bf
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-10-02 11:23:07 +0100

    Add article ID to pod mapping

--- server/mappings/article-pod-mapping.js
+++ server/mappings/article-pod-mapping.js
@@ -14,6 +14,7 @@ module.exports = function(article, imageOptions) {
 	};
 
 	var articleModel = {
+		id: article.item.id,
 		headline: {
 			text: article.item.title.title,
 			url: '/content/' + article.item.id
