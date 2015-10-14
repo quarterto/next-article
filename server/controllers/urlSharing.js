0d9b1e172fd6ff3345c99e02eb8f6bab48bd23de
Jake Champion <jakechampion.jake2@googlemail.com>
2015-10-14 15:22:34 +0100

    Use FT-USER-UUID header instead of reading a cookie

--- server/controllers/urlSharing.js
+++ server/controllers/urlSharing.js
@@ -1,12 +1,9 @@
 "use strict";
 
-var atob = require('base-64').decode;
-var R = require('ramda');
-
 var generateToken = require('../lib/generateToken');
 var decrypt = require('share-code-creator').decrypt;
 
-var shareCodeRegex = /[a-z0-9]{32}/
+var shareCodeRegex = /[a-z0-9]{32}/;
 var isShareCode = shareCodeRegex.test.bind(shareCodeRegex);
 
 module.exports = function (req, res, next) {
@@ -15,17 +12,10 @@ module.exports = function (req, res, next) {
 
 	if (isShareCode(code)) {
 		var sharingUserID = decrypt(code, article);
-		console.log("userID:", sharingUserID)
 
 		if (true) { // need to check the user ID is infact a user -- may want to check dam or subs to see if they are allowed to share the article and have tokens left
-			var hasFTUser = R.has('FT_User');
-			var getFTUser = R.prop('FT_User');
-			var individiualCookieProperties = R.split(':');
-			var isUserIDOrErightsID = prop => prop.startsWith('USERID') || prop.startsWith('ERIGHTSID');
-			var splitKeyAndValue = R.split('=');
-
-			if (hasFTUser(req.cookies)) { // if the user is logged in and can already view the article, no need to create a gift token
-				var userInfo = new Map(R.map(splitKeyAndValue, R.filter(isUserIDOrErightsID, individiualCookieProperties(getFTUser(req.cookies)))));
+			if (req.header('FT-User-UUID') {
+				// if the user is logged in and can already view the article, no need to create a gift token
 				return next();
 			} else {
 				var token = generateToken(article, res);
