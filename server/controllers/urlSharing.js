6522d3304436d44ec3fe4158aa1f481eb04e07da
Jake Champion <jakechampion.jake2@googlemail.com>
2015-10-15 10:48:10 +0100

    refactor

--- server/controllers/urlSharing.js
+++ server/controllers/urlSharing.js
@@ -1,31 +1,26 @@
 "use strict";
 
 var generateToken = require('article-token-creator');
-var decrypt = require('share-code-creator').decrypt;
+var shareCodeCreator = require('share-code-creator');
+var decrypt = shareCodeCreator.decrypt;
+var isShareCodePattern = shareCodeCreator.isShareCodePattern;
 var privateKey = process.env.TOKEN_GENERATING_PRIVATE_KEY;
-
-var shareCodeRegex = /[a-z0-9]{32}/;
-var isShareCode = shareCodeRegex.test.bind(shareCodeRegex);
 
 module.exports = function (req, res, next) {
 	var code = req.query.share_code;
 	var article = req.params.id;
 
-	if (isShareCode(code)) {
-		var sharingUserID = decrypt(code, article);
+	if (req.header('FT-User-UUID') === undefined || req.header('FT-User-UUID') === null) {
+		if (isShareCodePattern(code)) {
+			var sharingUserID = decrypt(code, article);
 
-		if (true) { // need to check the user ID is infact a user -- may want to check dam or subs to see if they are allowed to share the article and have tokens left
-			if (req.header('FT-User-UUID')) {
-				// if the user is logged in and can already view the article, no need to create a gift token
-				return next();
-			} else {
+			if (true) { // need to check the user ID is infact a user -- may want to check dam or subs to see if they are allowed to share the article and have tokens left
 				res.append('Set-Cookie', generateToken(article, privateKey));
 				return res.redirect('/content/' + req.params.id);
 			}
-		} else {
-			return next();
 		}
-	} else {
-		return next();
 	}
+
+	// if the user is logged in and can already view the article, no need to create a gift token
+	return next();
 };
