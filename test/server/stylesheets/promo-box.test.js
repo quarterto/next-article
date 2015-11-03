93a9a2b4175f9519a676cb120c8efa5cb9c9df7c
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-11-03 12:41:21 +0000

    Update promo box transform tests to work with img elements not ft-content

--- test/server/stylesheets/promo-box.test.js
+++ test/server/stylesheets/promo-box.test.js
@@ -11,7 +11,7 @@ describe('Promo-boxes', function() {
 			'<promo-box>' +
 				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/e7b203ac-2351-11e5-23e5-e651ba9c5bc5" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></promo-intro>' +
 			'</promo-box>'
 		)
@@ -25,12 +25,7 @@ describe('Promo-boxes', function() {
 							'</div>' +
 							'<div class="promo-box__headline">Greece debt crisis</div>' +
 							'<div class="promo-box__image">' +
-								'<picture data-image-set-id="e7b203ac-2351-11e5-23e5-e651ba9c5bc5" class="article__image n-image">' +
-									'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-									'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-									'<!--[if IE 9]></video><![endif]-->' +
-									'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-								'</picture>' +
+								'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=300">' +
 							'</div>' +
 							'<div class="promo-box__content">' +
 								'<div class="promo-box__content__initial">' +
@@ -49,7 +44,7 @@ describe('Promo-boxes', function() {
 			'<promo-box>' +
 				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/e7b203ac-2351-11e5-23e5-e651ba9c5bc5" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
 				'<p><strong>Best mentor:</strong> “Chris Baohm, my boss at Gresham Partners in Australia . . . made me understand the importance of breaking down complex situations into the core commercial objectives.”</p></promo-intro>' +
 			'</promo-box>'
@@ -64,12 +59,7 @@ describe('Promo-boxes', function() {
 							'</div>' +
 							'<div class="promo-box__headline">Greece debt crisis</div>' +
 							'<div class="promo-box__image">' +
-								'<picture data-image-set-id="e7b203ac-2351-11e5-23e5-e651ba9c5bc5" class="article__image n-image">' +
-									'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-									'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-									'<!--[if IE 9]></video><![endif]-->' +
-									'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-								'</picture>' +
+								'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=700">' +
 							'</div>' +
 							'<div class="promo-box__content">' +
 							'<div class="promo-box__content__initial"><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
@@ -86,7 +76,7 @@ describe('Promo-boxes', function() {
 			'<promo-box>' +
 				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/2889cee2-2fc7-11e5-0fca-327ba7efe7b6" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
 				'<p><strong>Best mentor:</strong> “Chris Baohm, my boss at Gresham Partners in Australia . . . made me understand the importance of breaking down complex situations into the core commercial objectives.”</p>' +
 				'<p><strong>Biggest mistake:</strong> “It became very clear that we needed a strong team with us who we could trust to navigate the huge due diligence tasks we had in several countries, in a different language.”</p>' +
@@ -103,12 +93,7 @@ describe('Promo-boxes', function() {
 							'</div>' +
 							'<div class="promo-box__headline">Greece debt crisis</div>' +
 							'<div class="promo-box__image">' +
-								'<picture data-image-set-id="2889cee2-2fc7-11e5-0fca-327ba7efe7b6" class="article__image n-image">' +
-									'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-									'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-									'<!--[if IE 9]></video><![endif]-->' +
-									'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-								'</picture>' +
+								'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=700">' +
 							'</div>' +
 							'<div class="promo-box__content o-expander__content">' +
 								'<div class="promo-box__content__initial"><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
@@ -127,7 +112,7 @@ describe('Promo-boxes', function() {
 		return transform(
 			'<promo-box>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/2889cee2-2fc7-11e5-0fca-327ba7efe7b6" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></promo-intro>' +
 			'</promo-box>'
 		)
@@ -141,12 +126,7 @@ describe('Promo-boxes', function() {
 						'</div>' +
 						'<div class="promo-box__headline">Greece debt crisis</div>' +
 						'<div class="promo-box__image">' +
-							'<picture data-image-set-id="2889cee2-2fc7-11e5-0fca-327ba7efe7b6" class="article__image n-image">' +
-								'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-								'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-								'<!--[if IE 9]></video><![endif]-->' +
-								'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-							'</picture>' +
+							'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=300">' +
 						'</div>' +
 						'<div class="promo-box__content">' +
 						'<div class="promo-box__content__initial"><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></div></div>' +
@@ -201,7 +181,7 @@ describe('Promo-boxes', function() {
 			'<promo-box>' +
 				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/2889cee2-2fc7-11e5-0fca-327ba7efe7b6" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
 				'<p><strong>Best mentor:</strong> “Chris Baohm, my boss at Gresham Partners in Australia . . . made me understand the importance of breaking down complex situations into the core commercial objectives.”</p>' +
 				'<p><strong>Biggest mistake:</strong> “It became very clear that we needed a strong team with us who we could trust to navigate the huge due diligence tasks we had in several countries, in a different language.”</p>' +
@@ -210,7 +190,7 @@ describe('Promo-boxes', function() {
 			'<promo-box>' +
 				'<promo-title><p>Tatomer Riesling 2012</p></promo-title>' +
 				'<promo-headline><p>Greece debt crisis</p></promo-headline>' +
-				'<promo-image><ft-content type="http://www.ft.com/ontology/content/ImageSet" url="http://api.ft.com/content/e7b203ac-2351-11e5-23e5-e651ba9c5bc5" data-embedded="true"></ft-content></promo-image>' +
+				'<promo-image><img src="http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c" /></promo-image>' +
 				'<promo-intro><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></promo-intro>' +
 			'</promo-box>'
 		)
@@ -222,12 +202,7 @@ describe('Promo-boxes', function() {
 							'<div class="promo-box__title"><div class="promo-box__title__name">Tatomer Riesling 2012</div></div>' +
 							'<div class="promo-box__headline">Greece debt crisis</div>' +
 							'<div class="promo-box__image">' +
-								'<picture data-image-set-id="2889cee2-2fc7-11e5-0fca-327ba7efe7b6" class="article__image n-image">' +
-									'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-									'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-									'<!--[if IE 9]></video><![endif]-->' +
-									'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-								'</picture>' +
+								'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=700">' +
 							'</div>' +
 							'<div class="promo-box__content o-expander__content">' +
 							'<div class="promo-box__content__initial"><p><strong>Breakthrough:</strong> “Closing our first seed round in 10 days three times oversubscribed gave us momentum [to carry through] to the execution of our strategy and into the IPO.”</p>' +
@@ -242,12 +217,7 @@ describe('Promo-boxes', function() {
 							'<div class="promo-box__title"><div class="promo-box__title__name">Tatomer Riesling 2012</div></div>' +
 							'<div class="promo-box__headline">Greece debt crisis</div>' +
 							'<div class="promo-box__image">' +
-								'<picture data-image-set-id="e7b203ac-2351-11e5-23e5-e651ba9c5bc5" class="article__image n-image">' +
-									'<!--[if IE 9]><video style="display: none;"><![endif]-->' +
-									'<source data-image-size="280" media="(min-width: 490px)"></source>' +
-									'<!--[if IE 9]></video><![endif]-->' +
-									'<img data-image-type="srcset" data-image-size="400" class="n-image__img" alt="">' +
-								'</picture>' +
+								'<img alt="" src="https://next-geebee.ft.com/image/v1/images/raw/http://com.ft.imagepublish.prod.s3.amazonaws.com/1871b094-3b7d-11e5-bbd1-b37bc06f590c?source=next&amp;fit=scale-down&amp;width=300">' +
 							'</div>' +
 							'<div class="promo-box__content">' +
 							'<div class="promo-box__content__initial"><p>Graham Tatomer worked at Austrian Riesling producer Emmerich Knoll and now fashions this example from the old vines of the Kick-on Ranch in Santa Barbara (£26.95, Roberson)</p></div></div>' +
