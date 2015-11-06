65d7152210017150e062e5e76266342586723512
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-11-06 12:38:46 +0000

    Update healthcheck to include ES V3 and remove CAPI

--- server/checks/main.js
+++ server/checks/main.js
@@ -1,52 +1,48 @@
 'use strict';
 
-var api = require('next-ft-api-client');
+const api = require('next-ft-api-client');
 
-const ARTICLE_ID = "d0377096-f290-11e4-b914-00144feab7de";
+const ARTICLE_ID = 'd0377096-f290-11e4-b914-00144feab7de';
 const INTERVAL = 60 * 1000;
 
 const statuses = {
 	elastic: {
 		v1: false,
-		v2: false
-	},
-	capi: {
-		v1: false,
-		v2: false
+		v2: false,
+		v3: false
 	},
 	livefyre: false
 };
 
 /**
- * Calls CAPI and elasticsearch for both V1 & V2 and updates [statuses]
+ * Calls Elasticsearch for V1, V2 and V3 and updates [statuses]
  * in the parent scope.
  * @method pingServices
  */
 function pingServices() {
-	[ {useElastic: true}, {useElastic: false} ]
-		.forEach(config => {
-			const backend = config.useElastic ? 'elastic' : 'capi';
 
-			api.contentLegacy({
-				uuid: ARTICLE_ID,
-				useElasticSearch: config.useElastic
-			})
-			.then(() => { statuses[backend].v1 = true; })
-			.catch(() => { statuses[backend].v1 = false; });
+	api.contentLegacy({
+		uuid: ARTICLE_ID
+	})
+		.then(() => { statuses.elastic.v1 = true; })
+		.catch(() => { statuses.elastic.v1 = false; });
 
-			api.content({
-				uuid: ARTICLE_ID,
-				type: 'Article',
-				metadata: true,
-				useElasticSearch: config.useElastic
-			})
-			.then(() => { statuses[backend].v2 = true; })
-			.catch(() => { statuses[backend].v2 = false; });
-	});
-	
+	api.content({
+		uuid: ARTICLE_ID,
+	})
+		.then(() => { statuses.elastic.v2 = true; })
+		.catch(() => { statuses.elastic.v2 = false; });
+
+	api.content({
+		uuid: ARTICLE_ID,
+		index: 'v3_api_v2'
+	})
+		.then(() => { statuses.elastic.v3 = true; })
+		.catch(() => { statuses.elastic.v3 = false; });
+
 	fetch('https://session-user-data.webservices.ft.com/v1/livefyre/init?title=Terror+must+not+trample+on+Tunisian+institutions+%E2%80%94+FT.com&url=https%3A%2F%2Fnext.ft.com%2Fcontent%2F4b949d2c-1fdc-11e5-ab0f-6bb9974f25d0&articleId=4b949d2c-1fdc-11e5-ab0f-6bb9974f25d0&el=comments&stream_type=livecomments&callback=jsonp_m49qbwvzpvi0&_=1444384681349')
-	.then((res) => { statuses['livefyre'] = res.ok; })
-	.catch(() => { statuses['livefyre'] = false; });
+	.then((res) => { statuses.livefyre = res.ok; })
+	.catch(() => { statuses.livefyre = false; });
 }
 
 /**
@@ -72,15 +68,13 @@ function livefyreStatus() {
 	return {
 		getStatus: () => ({
 			name: 'session-user-data.webservices.ft.com (livefyre) responded successfully.',
-			ok: statuses['livefyre'],
+			ok: statuses.livefyre,
 			businessImpact: 'Users may not see comments at bottom of article',
 			severity: 3,
 			technicalSummary: 'Fetches the session-user-data call used on the client side to initialise comments'
 		})
 	};
 }
-
-
 
 module.exports = {
 	init: function() {
@@ -89,7 +83,6 @@ module.exports = {
 	},
 	esv1: buildStatus('elastic', 'v1'),
 	esv2: buildStatus('elastic', 'v2'),
-	capiv1: buildStatus('capi', 'v1'),
-	capiv2: buildStatus('capi', 'v2'),
+	esv3: buildStatus('elastic', 'v3'),
 	livefyre: livefyreStatus()
 };
