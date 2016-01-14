b699cb0605857f9bf201d494a0f46ec1ba191a91
Tom Parker <tom.p.parker@googlemail.com>
2016-01-14 12:40:59 +0000

    Remove NPS feedback form

--- client/main.js
+++ client/main.js
@@ -4,7 +4,6 @@ require('next-js-setup').bootstrap(({flags}) => {
 
 	const myFtClient = require('next-myft-client');
 	const myFtUi = require('next-myft-ui');
-	const npsFeedback = require('n-nps-feedback');
 
 	const prompts = require('n-message-prompts');
 	const oViewport = require('o-viewport');
@@ -34,7 +33,6 @@ require('next-js-setup').bootstrap(({flags}) => {
 	myFtClient.init(clientOpts);
 
 	myFtUi.init({anonymous: !(/FTSession=/.test(document.cookie))});
-	npsFeedback.init();
 
 	layout.init(flags);
 
