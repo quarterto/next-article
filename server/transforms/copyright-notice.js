1744a945c84f48c0717773d60078657bf6ba847e
Matt Andrews <matt@mattandre.ws>
2016-01-29 16:38:37 +0000

    Add ‘All rights reserved’ to the copyright notice

--- server/transforms/copyright-notice.js
+++ server/transforms/copyright-notice.js
@@ -5,7 +5,7 @@ module.exports = function() {
   const thisYear = new Date().getFullYear();
   const copyrightNotice = [
     '<p class="article__copyright-notice"><a href="http://www.ft.com/servicestools/help/copyright" data-trackable="link-copyright">Copyright</a>',
-    ` The Financial Times Limited ${thisYear}. You may share using our article tools.`,
+    ` The Financial Times Limited ${thisYear}. All rights reserved. You may share using our article tools.`,
     ' Please don\'t cut articles from FT.com and redistribute by email or post to the web.</p>'
   ].join('');
 
