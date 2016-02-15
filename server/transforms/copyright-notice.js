c0a19e3c8a2aca2f20191d2be837fea03f9aa573
andygnewman <andrew.newman@ft.com>
2016-02-15 14:28:57 +0000

    refactor copyright styling

--- server/transforms/copyright-notice.js
+++ server/transforms/copyright-notice.js
@@ -4,7 +4,7 @@ module.exports = function() {
 
   const thisYear = new Date().getFullYear();
   const copyrightNotice = [
-    '<p class="copyright-notice"><a href="http://www.ft.com/servicestools/help/copyright" data-trackable="link-copyright">Copyright</a>',
+    '<p class="n-content-copyright"><a href="http://www.ft.com/servicestools/help/copyright" data-trackable="link-copyright">Copyright</a>',
     ` The Financial Times Limited ${thisYear}. All rights reserved. You may share using our article tools.`,
     ' Please don\'t cut articles from FT.com and redistribute by email or post to the web.</p>'
   ].join('');
