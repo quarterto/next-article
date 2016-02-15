81744bbde75276cb3bb11220ce0116e53ca063d2
andygnewman <andrew.newman@ft.com>
2016-02-15 16:27:01 +0000

    further tag style refactoring

--- server/transforms/byline.js
+++ server/transforms/byline.js
@@ -5,7 +5,7 @@ module.exports = (byline, authors) => {
 		authors.forEach(author => {
 			byline = byline.replace(
 				new RegExp('\\b(' + author.name + ')\\b'),
-				'<a class="n-content-tag n-content-tag--link" href="/stream/authorsId/' + author.id + '" data-trackable="author">$1</a>'
+				'<a class="n-content-tag" href="/stream/authorsId/' + author.id + '" data-trackable="author">$1</a>'
 			);
 		});
 	}
